7c558b01184ebd3ac219ff8b61180d7f2e73a659
SeaFilmz <danielschmidt18@gmail.com>
2020-05-22 20:16:59 -0700

    Mobile Menu PHP File
    A seperate PHP file with my mobile menu that I can link to on my different website pages.

---
+++ sfmobilemenu.php
@@ -0,0 +1,18 @@
+      <div class="mobile-menu" onclick="onClickMenu()">
+        <span class="bar1"></span>
+        <span class="bar2"></span>
+        <span class="bar3"></span>
+      </div>
+    
+      <div class="MobileNav">
+        <p><a href="sfseattlemovies.php">Movies Filmed in Seattle by Title</a></p>
+        <p><a href="sfseattlemoviesyear.php">Movies Filmed in Seattle by Year Released</a></p>
+        <p><a href="sfseattlemoviesruntime.php">Movies Filmed in Seattle by Runtime</a></p>
+        <p><a href="sfseattlemoviesgross.php">Movies Filmed in Seattle by Total Worldwide Gross</a></p>
+        <p><a href="sfseattleactors.php">Seattle Born Actors by First Name</a></p>
+        <p><a href="sfseattleactorsbirthdate.php">Actors Born in Seattle by Birthdate</a></p>
+        <p><a href="sfseattlemusicians.php">Seattle Born Musicians</a></p>
+        <p><a href="sfseattleathletes.php">Seattle Born Athletes</a></p>
+        <p><a href="seattlefunfacts.php">Seattle Facts</a></p>
+        <p><a href="seattlebordercities.php">Cities Near Seattle</a></p>
+      </div>
