6e627f4ed5c81831bd250117423fc793f4c6cbf6
SeaFilmz <danielschmidt18@gmail.com>
2020-05-26 15:12:23 -0700

    Total Movies Count for Seattle PHP File
    A seperate PHP file with my total movies count for Seattle that I can link to when I need it to be displayed on my website.

---
+++ sfmoviescount.php
@@ -0,0 +1,38 @@
+        <div class="MCTable">
+        <table class="MovieCountTable">
+          <tr>
+            <th class="MovieCountRowHeader">Total Movies</th>       
+        
+        <?php
+            // 2. Perform database query
+            $query = "SELECT COUNT(*) ";
+            $query .= "FROM movies ";
+            $result = mysqli_query($connection, $query);
+            //Test if there was a query error   
+            if (!$result) {
+                die("Database query failed.");
+            } /* else { 
+                echo "Connected";
+            } */
+        ?>
+
+        <?php
+            // 3. Use returned data (if any) 
+            while($movies = mysqli_fetch_assoc($result)) {
+                // output data from each row
+        ?>
+
+            <td class="MovieCountNumber"><?php echo $movies["COUNT(*)"]; ?></td>
+          </tr>
+          
+        <?php
+            }
+        ?>
+
+        <?php
+            // 4. Release returned data
+            mysqli_free_result($result);
+        ?>
+
+        </table>
+        </div>
