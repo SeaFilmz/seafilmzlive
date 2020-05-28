121b1fd6a00875a202810be2ad89d46fc6dfb51f
SeaFilmz <danielschmidt18@gmail.com>
2020-05-27 21:15:48 -0700

    Added by Year List to Movies Filmed in Seattle by Title
    Used HTML, PHP and a new SQL query to help add a new table for movies filmed in Seattle by title.

--- sfseattlemovies.php
+++ sfseattlemovies.php
@@ -15,7 +15,7 @@
   </head>
     
   <body>
-    <header id="MovieTitleTop" class="banner">	
+    <header id="MovieTitleTop" class="banner">
       <h1><a href="index.php">SeaFilmz</a></h1>
       <b class="solgan">Your Seattle Film and Data Connection</b>
 
@@ -101,6 +101,58 @@
         ?>
 
         <?php
+            // 4. Release returned data
+            mysqli_free_result($result);
+        ?>
+
+        </table>
+        </div>
+
+        <!--link to Total Movie Count-->
+<?php require("sfmoviescount.php"); ?>
+  
+      <h2 class="MoviesPageHeader"><b>Movies Filmed in Seattle by Year</b></h2>
+
+        <div class="MYTable">
+        <table class="MovieYearTable">
+          <tr>
+            <th class="MovieYearColumnHeader1">Title</th>
+            <th class="MovieYearColumnHeader2">Year</th>
+          </tr>
+
+        <?php
+            // 2. Perform database query
+            $query = "SELECT * ";
+            $query .= "FROM moviesfilminglocation ";
+            $query .= "INNER JOIN movies ";
+            $query .= "ON movies.MovieID = moviesfilminglocation.MovieID ";
+            $query .= "INNER JOIN filminglocations ";
+            $query .= "ON filminglocations.FilmingLocationID = moviesfilminglocation.FilmingLocationID ";
+            $query .= "WHERE City = 'Seattle' ";
+            $query .= "ORDER BY YearReleased DESC, MovieTitle ";
+            $result = mysqli_query($connection, $query);
+            //Test if there was a query error
+            if (!$result) {
+                die("Database query failed.");
+            }
+        ?>
+    
+        <?php
+            // 3. Use returned data (if any)
+            while($movies = mysqli_fetch_assoc($result)) {
+                // output data from each row
+        ?>
+
+          <tr class="MoviesContent">
+            <td class="MovieTitlesContent"><b><a href= "<?php echo $movies["MoviePageLink"]; ?>"><?php echo $movies["MovieTitle"]; ?></a></b></td>
+            <td class="MovieYearContent"><?php echo $movies["YearReleased"]; ?></td>
+          </tr>
+          
+        <?php
+            }
+        ?>
+
+        <?php    
             // 4. Release returned data
             mysqli_free_result($result);
         ?>
