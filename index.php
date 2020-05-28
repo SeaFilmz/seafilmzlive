69090263292d97eb3f93086593f29d37897c863a
SeaFilmz <danielschmidt18@gmail.com>
2020-05-28 13:48:45 -0700

    Clean White Space Home Page
    Cleaned up white space in my code for the home page.

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php require_once("db_connection.php"); ?>
 <!DOCTYPE html>
-<html lang="en"> 
+<html lang="en">
 
   <head>
     <!--Connection to Google Analytics.-->
     <?php include "googleanalytics_connection.php"; ?>
-      
+
     <meta charset="utf-8">
     <meta name="description" content="SeaFilmz is your Seattle media connection with a focus on film.">
     <title>SeaFilmz - Greater Seattle Film, Media, Data</title>
@@ -13,9 +13,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="sfmain.js" defer></script>
   </head>
-    
+
   <body class="MainPage">
-    <header id="MainTop" class="banner">	
+    <header id="MainTop" class="banner">
       <h1><a href="index.php">SeaFilmz</a></h1>
       <b class="solgan">Your Seattle Film and Data Connection</b>
 
@@ -56,77 +56,77 @@
         </div>
       </nav>
     </header>
-          
-        <?php         
+
+        <?php
             // 4. Release returned data
             mysqli_free_result($result);
         ?>
-        
+
     <div class="MainPageContent">
       <p class="MainPageAbout">SeaFilmz is a Seattle media and data tool with a focus on movies.</p>
-            
+
       <p class="SeattleMoviesMain"><a href="sfseattlemovies.php">Movies Filmed in Seattle by Title</a></p>
-            
+
       <p class="SeattleMoviesMain"><a href="sfseattlemoviesyear.php">Movies Filmed in Seattle by Year Released</a></p>
-            
+
       <p class="SeattleMoviesMain"><a href="sfseattlemoviesruntime.php">Movies Filmed in Seattle by Runtime</a></p>
-            
+
       <p class="SeattleMoviesMain"><a href="sfseattlemoviesgross.php">Movies Filmed in Seattle by Total Worldwide Gross</a></p>
-            
+
       <p class="SeattleActorsMain"><a href="sfseattleactors.php">Actors Born in Seattle by First Name</a></p>
-            
+
       <p class="SeattleActorsMain"><a href="sfseattleactorsbirthdate.php">Actors Born in Seattle by Birthdate</a></p>
-            
+
       <p class="SeattleMusiciansMain"><a href="sfseattlemusicians.php">Musicians Born in Seattle</a></p>
-            
+
       <p class="SeattleAthletesMain"><a href="sfseattleathletes.php">Athletes Born in Seattle</a></p>
-            
+
       <p class="SeattleFunFactsMain"><a href="seattlefunfacts.php">Seattle Fun Facts</a></p>
-            
+
       <p class="SeattleBorderCitiesMain"><a href="seattlebordercities.php">Seattle Border Cities</a></p>
     </div>
 
     <div class="sfQuizBackground">
 
       <h2 class="sfQuizHeader">SeaFilmz Quiz</h2>
-           
-      <form class="sfQuiz">        
-          
+
+      <form class="sfQuiz">
+
         <div class="sfQText"><p class="sfQ"><b>Sleepless in Seattle is the highest grossing movie flimed in Seattle.</b></p></div>
-      
+
           <input type="radio" name="answer1" value="true" class="trueAnswer1"> True <br>
           <input type="radio" name="answer1" value="false" class="falseAnswer1"> False <br>
-            
-        <p class="sfQ"><b>The Ring was released in 2002.</b></p>        
-      
+
+        <p class="sfQ"><b>The Ring was released in 2002.</b></p> 
+
           <input type="radio" name="answer2" value="true" class="trueAnswer2"> True <br>
           <input type="radio" name="answer2" value="false" class="falseAnswer2"> False <br>
-            
-        <p class="sfQ"><b>10 Things I Hate About You cast included Naomi Watts.</b></p>        
-      
+
+        <p class="sfQ"><b>10 Things I Hate About You cast included Naomi Watts.</b></p>
+
           <input type="radio" name="answer3" value="true" class="trueAnswer3"> True <br>
-          <input type="radio" name="answer3" value="false" class="falseAnswer3"> False <br>         
-            
+          <input type="radio" name="answer3" value="false" class="falseAnswer3"> False <br>      
+
       </form>
-        
+
       <div class="sfQuizButton">
         <button onclick="quizButton()" class="sfQuizButton"><b>Answer</b></button>
       </div>
-        
+
       <p id="answerText" class="answerText"></p>
-        
+
     </div>
-        
+
     <footer>
-      <nav class="navigation"> 
+      <nav class="navigation">
         <ul>
           <li class="NavFooterMobile"><a href="#MainTop">Go to Top</a></li>
-    
+
           <?php require_once("sffootermenu.php"); ?>
         </ul>
       </nav>
     </footer>
-        
+
   </body>
 
 </html>
