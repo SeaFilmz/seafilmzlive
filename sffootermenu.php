d9786d87c09f1b92235ffec3fb38bc3087afde1e
SeaFilmz <danielschmidt18@gmail.com>
2020-05-28 23:55:23 -0700

    Clean White Space Footer Menu PHP File
    Cleaned up white space in the seperate PHP file code for that part of my footer menu.

--- sffootermenu.php
+++ sffootermenu.php
@@ -1,5 +1,5 @@
 <li class="NavFooterMobile" style="float:right"><a href="sfcontact.php">Contact</a></li>
-            
+
           <li class="NavFooterMobile" style="float:right"><a href="sfservices.php">Services</a></li>
-            
+
           <li class="NavFooterMobile" style="float:right"><a href="sfabout.php">About</a></li>
