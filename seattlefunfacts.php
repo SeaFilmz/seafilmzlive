a3eadb26f3bec77fb884b221436e36f617785361
SeaFilmz <danielschmidt18@gmail.com>
2020-05-28 20:17:09 -0700

    Clean White Space Seattle Facts Page
    Cleaned up white space in my code for the Seattle fact page.

--- seattlefunfacts.php
+++ seattlefunfacts.php
@@ -1,6 +1,6 @@
 <?php require_once("db_connection.php"); ?>
 <!DOCTYPE html>
-<html lang="en"> 
+<html lang="en">
 
   <head>
     <!--Connection to Google Analytics.-->
@@ -13,9 +13,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script src="sfmain.js" defer></script>
   </head>
-    
+
   <body>
-    <header id="SeattleFFTop" class="banner">	
+    <header id="SeattleFFTop" class="banner">
       <h1><a href="index.php">SeaFilmz</a></h1>
       <b class="solgan">Your Seattle Film and Data Connection</b>
 
@@ -56,31 +56,31 @@
         </div>
       </nav>
     </header>
-        
+
     <h2 class="SeattleFFHeader">Seattle Fun Facts</h2>
-        
+
     <div class="SeattleFFContent">
-      <p><b>Officially Became a City:</b> December 2, 1869</p>		      
+      <p><b>Officially Became a City:</b> December 2, 1869</p>	      
       <p><b>County:</b> <a href="http://www.kingcounty.gov/">King</a></p>
       <p><b>State:</b> <a href="http://access.wa.gov/">Washington</a></p>
       <p><b>Country:</b> <a href="https://www.usa.gov/">United States of America</a></p>
       <p><b>Website:</b> <a href="http://www.seattle.gov/">www.seattle.gov</a></p>
       <p><b>Movies Filmed in Seattle:</b> <a href="sfseattlemovies.php">List of Movies Filmed in Seattle</a>
       <p><b>Movie Theaters in Seattle:</b> <a href="seattlemovietheaters.php">List of Movie Theaters in Seattle</a></p>
-      <p><b>Professional Sports Teams:</b> 
-        <a href="http://www.seahawks.com/">Seattle Seahawks</a> (men's football), 
-        <a href="http://storm.wnba.com/">Seattle Storm</a> (women's basketball), 
-        <a href="https://www.mlb.com/mariners">Seattle Mariners</a> (men's baseball), 
-        <a href="https://www.soundersfc.com/">Seattle Sounders FC</a> (men's soccer), 
+      <p><b>Professional Sports Teams:</b>
+        <a href="http://www.seahawks.com/">Seattle Seahawks</a> (men's football),
+        <a href="http://storm.wnba.com/">Seattle Storm</a> (women's basketball),
+        <a href="https://www.mlb.com/mariners">Seattle Mariners</a> (men's baseball),
+        <a href="https://www.soundersfc.com/">Seattle Sounders FC</a> (men's soccer),
         <a href="https://www.reignfc.com/">Seattle Reign FC</a> (women's soccer),
         <a href="https://www.xfl.com/en-US/teams/seattle">Seattle Dragons</a> (men's football)
       </p>
       <p><b>Universities or Colleges:</b> 
         <a href="https://www.washington.edu/">University of Washington</a>,
-        <a href="https://www.seattleu.edu/">Seattle University</a>, 
-        <a href="http://spu.edu/">Seattle Pacific University</a>, 
-        <a href="https://www.northseattle.edu/">North Seattle College</a>, 
-        <a href="http://www.seattlecentral.org/">Seattle Central College</a>, 
+        <a href="https://www.seattleu.edu/">Seattle University</a>,
+        <a href="http://spu.edu/">Seattle Pacific University</a>,
+        <a href="https://www.northseattle.edu/">North Seattle College</a>,
+        <a href="http://www.seattlecentral.org/">Seattle Central College</a>,
         <a href="http://www.southseattle.edu/">South Seattle College</a>
       </p>
       <p><b>Libraries in Seattle:</b> <a href="https://www.spl.org/hours-and-locations">List of Libraries in Seattle</a></p>
@@ -93,13 +93,13 @@
       </p>
       <p><b>Bowling Alley:</b> <a href="https://www.garagebilliards.com/">Garage</a>, <a href="http://www.wsbowl.com/">West Seattle Bowl</a></p>
     </div>
-		  
+
 		<div class="SeattleFFImage">
 		  <p class="PopsicleText">Where in Seattle is this popsicle locationed?</p>
 		  <img class="PopsicleSculpture" src="popsiclesculpture.JPG" alt="Giant red popsicle located in the city of Seattle">
 		</div>
-        
-        <?php       
+
+        <?php
             // 4. Release returned data
             mysqli_free_result($result);
         ?>
@@ -109,7 +109,7 @@
     </div>
 
     <footer>
-      <nav class="navigation"> 
+      <nav class="navigation">
         <ul>
           <li class="NavFooterMobile"><a href="#SeattleFFTop">Go to Top</a></li>
 
@@ -117,7 +117,7 @@
           <?php require_once("sffootermenu.php"); ?>
         </ul>
       </nav>
-    </footer>  
+    </footer>
 
   </body>
 
