9934310196af542eecb986a48de07b0da865d02b
SeaFilmz <danielschmidt18@gmail.com>
2020-06-01 19:28:19 -0700

    Desktop and Mobile Menu
    A seperate PHP file with my desktop and mobile menu that I can link to on my different website pages.

---
+++ sfmenu.php
@@ -0,0 +1,52 @@
+      <div class="mobile-menu" onclick="onClickMenu()">
+        <span class="bar1"></span>
+        <span class="bar2"></span>
+        <span class="bar3"></span>
+      </div>
+
+      <div class="MobileNav">
+        <p><a href="sfseattlemovies.php">Movies Filmed in Seattle by Title</a></p>
+        <p><a href="sfseattlemoviesyear.php">Movies Filmed in Seattle by Year Released</a></p>
+        <p><a href="sfseattlemoviesruntime.php">Movies Filmed in Seattle by Runtime</a></p>
+        <p><a href="sfseattlemoviesgross.php">Movies Filmed in Seattle by Total Worldwide Gross</a></p>
+        <p><a href="sfseattleactors.php">Seattle Born Actors by First Name</a></p>
+        <p><a href="sfseattleactorsbirthdate.php">Actors Born in Seattle by Birthdate</a></p>
+        <p><a href="sfseattlemusicians.php">Seattle Born Musicians</a></p>
+        <p><a href="sfseattleathletes.php">Seattle Born Athletes</a></p>
+        <p><a href="seattlefunfacts.php">Seattle Facts</a></p>
+        <p><a href="seattlebordercities.php">Cities Near Seattle</a></p>
+      </div>
+
+      <nav class="navigation">
+        <div class="dropdown">
+          <span class="NavMobile">Movies Filmed in Seattle</span><div class="upsideDownTriangle"></div>
+          <div class="dropdown-content">
+            <p><a href="sfseattlemovies.php">by Title</a></p>
+            <p><a href="sfseattlemoviesyear.php">by Year Released</a></p>
+            <p><a href="sfseattlemoviesruntime.php">by Runtime</a></p>
+            <p><a href="sfseattlemoviesgross.php">by Total Worldwide Gross</a></p>
+          </div>
+        </div>
+        <div class="dropdown">
+          <span class="NavMobile">Seattle Born</span><div class="upsideDownTriangle"></div>
+          <div class="dropdown-content">
+            <p><a href="sfseattleactors.php">Actors</a></p>
+            <p><a href="sfseattlemusicians.php">Musicians</a></p>
+            <p><a href="sfseattleathletes.php">Athletes</a></p>
+          </div>
+        </div>
+        <div class="dropdown">
+          <span class="NavMobile">City Facts</span><div class="upsideDownTriangle"></div>
+          <div class="dropdown-content">
+            <p><a href="seattlefunfacts.php">Seattle</a></p>
+            <p><a href="shoreline.php">Shoreline</a></p>
+            <p><a href="lfp.php">Lake Forest Park</a></p>
+            <p><a href="tukwila.php">Tukwila</a></p>
+            <p><a href="burien.php">Burien</a></p>
+            <p><a href="seatac.php">SeaTac</a></p>
+            <p><a href="mercerisland.php">Mercer Island</a></p>
+            <p><a href="medina.php">Medina</a></p>
+            <p><a href="bellevue.php">Bellevue</a></p>
+          </div>
+        </div>
+      </nav>
