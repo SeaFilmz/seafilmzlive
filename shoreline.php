67592502326a300d7b2c0fcab61041f0873765c3
SeaFilmz <danielschmidt18@gmail.com>
2020-06-02 22:56:35 -0700

    Fixed Header Menu Connection Shoreline Facts Page
    Removed desktop header menu code from this file and linked to a new seperate file with the desktop and mobile header menu code.

--- shoreline.php
+++ shoreline.php
@@ -19,42 +19,9 @@
       <a href="index.php"><h1>SeaFilmz</h1></a>
       <b class="solgan">Your Seattle Film and Data Connection</b>
 
-    link to mobile menu
-<?php require_once("sfmobilemenu.php"); ?>
+    <!--link to desktop and mobile menu in header-->
+<?php require_once("sfmenu.php"); ?>
 
-      <nav class="navigation">
-        <div class="dropdown">
-          <span class="NavMobile">Movies Filmed in Seattle</span><div class="upsideDownTriangle"></div>
-          <div class="dropdown-content">
-            <p><a href="sfseattlemovies.php">by Title</a></p>
-            <p><a href="sfseattlemoviesyear.php">by Year Released</a></p>
-            <p><a href="sfseattlemoviesruntime.php">by Runtime</a></p>
-            <p><a href="sfseattlemoviesgross.php">by Total Worldwide Gross</a></p>
-          </div>
-        </div>
-        <div class="dropdown">
-          <span class="NavMobile">Seattle Born</span><div class="upsideDownTriangle"></div>
-          <div class="dropdown-content">
-            <p><a href="sfseattleactors.php">Actors</a></p>
-            <p><a href="sfseattlemusicians.php">Musicians</a></p>
-            <p><a href="sfseattleathletes.php">Athletes</a></p>
-          </div>
-        </div>
-        <div class="dropdown">
-          <span class="NavMobile">City Facts</span><div class="upsideDownTriangle"></div>
-          <div class="dropdown-content">
-            <p><a href="seattlefunfacts.php">Seattle</a></p>
-            <p><a href="shoreline.php">Shoreline</a></p>
-            <p><a href="lfp.php">Lake Forest Park</a></p>
-            <p><a href="tukwila.php">Tukwila</a></p>
-            <p><a href="burien.php">Burien</a></p>
-            <p><a href="seatac.php">SeaTac</a></p>
-            <p><a href="mercerisland.php">Mercer Island</a></p>
-            <p><a href="medina.php">Medina</a></p>
-            <p><a href="bellevue.php">Bellevue</a></p>
-          </div>
-        </div>
-      </nav>
     </header>
 
     <h2 class="ShorelineHeader">City of Shoreline (Northwest Bordering City of Seattle)</h2>
