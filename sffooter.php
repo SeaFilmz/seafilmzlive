d79775e8c86b3abc657f9af859c602a37d5b080a
SeaFilmz <danielschmidt18@gmail.com>
2020-06-21 07:04:18 -0700

    Full Footer Menu PHP File
    A seperate PHP file with all of my footer menu that I can link to on my different website pages.

---
+++ sffooter.php
@@ -0,0 +1,13 @@
+    <footer>
+      <nav class="navigation">
+        <ul>
+          <li class="NavFooterMobile"><a href="#goToTopLink">Go to Top</a></li>
+
+          <li class="NavFooterMobile" style="float:right"><a href="sfcontact.php">Contact</a></li>
+
+          <li class="NavFooterMobile" style="float:right"><a href="sfservices.php">Services</a></li>
+
+          <li class="NavFooterMobile" style="float:right"><a href="sfabout.php">About</a></li>
+        </ul>
+      </nav>
+    </footer>
