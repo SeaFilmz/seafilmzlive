2e0514a5854383a16da79cfac3fbfffa8dda59d9
SeaFilmz <danielschmidt18@gmail.com>
2020-07-03 23:39:17 -0700

    Added Website Name Text Change
    Added JavaScript code to tell people what the website name was short for.

--- sfmain.js
+++ sfmain.js
@@ -3,3 +3,15 @@ function onClickMenu() {
   document.querySelector(".mobile-menu").classList.toggle("change");
   document.querySelector(".MobileNav").classList.toggle("change");
 }
+
+//Title Stand For Button on About Page
+const websiteHeader = document.querySelector("#headerStandFor");
+
+function headerSwitchText() {
+    if (websiteHeader.innerHTML === "SeaFilmz") {
+        websiteHeader.innerHTML = "Seattle Filmz";
+    }
+    else {
+        websiteHeader.innerHTML = "SeaFilmz";
+    }
+}
