b388af7e2a3d387335a7aa1f5258274aa029cdd0
SeaFilmz <danielschmidt18@gmail.com>
2020-07-07 20:35:19 -0700

    Updated Links in Footer
    Updated the links in footer by droping the file extertion because the website does not need them any more.

--- sffooter.php
+++ sffooter.php
@@ -3,11 +3,11 @@
         <ul>
           <li class="NavFooterMobile"><a href="#goToTopLink">Go to Top</a></li>
 
-          <li class="NavFooterMobile" style="float:right"><a href="sfcontact.php">Contact</a></li>
+          <li class="NavFooterMobile" style="float:right"><a href="sfcontact">Contact</a></li>
 
-          <li class="NavFooterMobile" style="float:right"><a href="sfservices.php">Services</a></li>
+          <li class="NavFooterMobile" style="float:right"><a href="sfservices">Services</a></li>
 
-          <li class="NavFooterMobile" style="float:right"><a href="sfabout.php">About</a></li>
+          <li class="NavFooterMobile" style="float:right"><a href="sfabout">About</a></li>
         </ul>
       </nav>
     </footer>
