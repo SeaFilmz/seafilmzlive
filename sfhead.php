8f346194b5b44192f96e9de8cf57c2168f8f2f53
SeaFilmz <danielschmidt18@gmail.com>
2020-07-09 20:09:34 -0700

    Head PHP File
    A seperate PHP file with part of my webpages head section that I can link to on my different website pages.

---
+++ sfhead.php
@@ -0,0 +1,9 @@
+    <meta charset="utf-8">
+    <link type="text/css" rel="stylesheet" href="sfcss.css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="sfmain.js" defer></script>
+
+    <!--Connection to Google Analytics.-->
+    <?php include "googleanalytics_connection.php"; ?>
+    
+  </head>
