f2b8422bc055661eef840a295b0795cea190421f
SeaFilmz <danielschmidt18@gmail.com>
2020-07-10 18:55:43 -0700

    Updated Header Actors Born in Seattle
    Removed part of the head section and linked to a new seperate file with that part of the head section to make this webpage more dynamic.

--- sfseattleactors.php
+++ sfseattleactors.php
@@ -3,16 +3,12 @@
 <html lang="en">
 
   <head>
-    <!--Connection to Google Analytics.-->
-    <?php include "googleanalytics_connection.php"; ?>
-
-    <meta charset="utf-8">
-    <meta name="description" content="List of actors born is the city of Seattle organized by first name.">
     <title>Seattle Actors by First Name- SeaFilmz</title>
-    <link type="text/css" rel="stylesheet" href="sfcss.css" />
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <script src="sfmain.js" defer></script>
-  </head>
+    <meta name="description" content="List of actors born is the city of Seattle organized by first name.">
+
+    <!--link to part of my head-->
+<?php require_once "sfhead.php"; ?>
+
 
   <body>
     <!--link to header-->
