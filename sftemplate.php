417945de51a07d8bef046f78095db2eb366bd59d
SeaFilmz <danielschmidt18@gmail.com>
2020-07-11 15:30:49 -0700

    Start of PHP Website Template
    A seperate PHP file with the start of a template for the structure of the different webpages on seafilmz which they can link to. This template in return will make the website more dynamic.

---
+++ sftemplate.php
@@ -0,0 +1,13 @@
+<?php require_once("db_connection.php"); ?>
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <title><?php echo $title; ?></title>
+    <meta charset="utf-8">
+    <link type="text/css" rel="stylesheet" href="sfcss.css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="sfmain.js" defer></script>
+
+    <!--Connection to Google Analytics.-->
+    <?php include "googleanalytics_connection.php"; ?>
