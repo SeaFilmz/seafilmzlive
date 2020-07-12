f6ce52377175db71569e5d94cb4fc451e50a7c96
SeaFilmz <danielschmidt18@gmail.com>
2020-07-12 13:06:15 -0700

    Updated Other Washington State Cities Page with SF Webpage Template
    Updated the start of the other Washington state cities page to use the sftemplate.php file to make this page more dynamic.

--- washingtoncities.php
+++ washingtoncities.php
@@ -1,15 +1,10 @@
-<?php require_once("db_connection.php"); ?>
-<!DOCTYPE html>
-<html lang="en">
+<!--link to the start of a seafilmz general webpage template-->
+<?php $title = "Washington State Cities - SeaFilmz"; ?>
+<?php require_once "sftemplate.php"; ?>
 
 
-<head>
-  <title>Washington State Cities - SeaFilmz</title>
-  <meta name="description" content="SeaFilmz is your Seattle media connection with a focus on film.">
-
-  <!--link to part of my head-->
-<?php require_once "sfhead.php"; ?>
-
+    <meta name="description" content="SeaFilmz is your Seattle media connection with a focus on film.">
+  </head>
 
   <body>
     <!--link to header-->
