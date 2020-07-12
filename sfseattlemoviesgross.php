7bce349e1ac8e529c642e01c292ec7ae55f131e7
SeaFilmz <danielschmidt18@gmail.com>
2020-07-12 09:58:25 -0700

    Updated Movies Filmed Seattle Gross with SF Webpage Template
    Updated the start of the movies filmed in Seattle by gross page to use the sftemplate.php file to make this page more dynamic with more reusable code.

--- sfseattlemoviesgross.php
+++ sfseattlemoviesgross.php
@@ -1,14 +1,10 @@
-<?php require_once("db_connection.php"); ?>
-<!DOCTYPE html>
-<html lang="en">
+<!--link to the start of a seafilmz general webpage template-->
+<?php $title = "Seattle Movies by Box Office Gross - SeaFilmz"; ?>
+<?php require_once "sftemplate.php"; ?>
 
-  <head>
-    <title>Seattle Movies by Box Office Gross - SeaFilmz</title>
+
     <meta name="description" content="List of movies filmed fully or partly in the city of Seattle organized by total worldwide gross in US dollars.">
-
-    <!--link to part of my head-->
-<?php require_once "sfhead.php"; ?>
-
+  </head>
 
   <body>
     <!--link to header-->
