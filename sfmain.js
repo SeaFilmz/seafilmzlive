3d63fe4f5ea2d4845ab4231ebb222dc2ac4740f6
SeaFilmz <danielschmidt18@gmail.com>
2020-07-17 16:57:58 -0700

    Added SeaFilmz Quiz JavaScript
    Added JavaScript code for the actions of a SeaFilmz quiz on the home page.

--- sfmain.js
+++ sfmain.js
@@ -1,6 +1,6 @@
 //Mobile Menu Toggle
 function onClickMenu() {
-  document.querySelector(".mobile-menu").classList.toggle("change");
+	document.querySelector(".mobile-menu").classList.toggle("change");
   document.querySelector(".MobileNav").classList.toggle("change");
 }
 
@@ -8,10 +8,67 @@ function onClickMenu() {
 const websiteHeader = document.querySelector("#headerStandFor");
 
 function headerSwitchText() {
-    if (websiteHeader.innerHTML === "SeaFilmz") {
-        websiteHeader.innerHTML = "Seattle Filmz";
-    }
-    else {
-        websiteHeader.innerHTML = "SeaFilmz";
-    }
+  if (websiteHeader.innerHTML === "SeaFilmz") {
+    websiteHeader.innerHTML = "Seattle Filmz";
+  }
+  else {
+    websiteHeader.innerHTML = "SeaFilmz";
+  }
+}
+
+//Home Page Movie Quiz
+const trueButton1 = document.querySelector(".trueAnswer1");
+const falseButton1 = document.querySelector(".falseAnswer1");
+const trueButton2 = document.querySelector(".trueAnswer2");
+const falseButton2 = document.querySelector(".falseAnswer2");
+const trueButton3 = document.querySelector(".trueAnswer3");
+const falseButton3 = document.querySelector(".falseAnswer3");
+const answerTextConnect = document.querySelector("#answerText");
+
+function quizButton() {
+  if (trueButton1.checked && trueButton2.checked && trueButton3.checked) {
+    answerTextConnect.innerHTML = "Wrong - The highest grosing movie filmed in Seattle is The Ring. <br> Correct - The Ring was released in 2002. <br> Wrong - 10 Things I Hate About You cast does not included Naomi Watts.";
+  }
+  else if (falseButton1.checked && falseButton2.checked && falseButton3.checked) {
+    answerTextConnect.innerHTML = "Correct - The highest grosing movie filmed in Seattle is The Ring. <br> Wrong - The Ring was released in 2002. <br> Correct - 10 Things I Hate About You cast does not included Naomi Watts.";
+  }
+  else if (trueButton1.checked && trueButton2.checked && falseButton3.checked) {
+    answerTextConnect.innerHTML = "Wrong - The highest grosing movie filmed in Seattle is The Ring. <br> Correct - The Ring was released in 2002. <br> Correct - 10 Things I Hate About You cast does not included Naomi Watts.";
+  }
+  else if (falseButton1.checked && falseButton2.checked && trueButton3.checked) {
+    answerTextConnect.innerHTML = "Correct - The highest grosing movie filmed in Seattle is The Ring. <br> Wrong - The Ring was released in 2002. <br> Wrong - 10 Things I Hate About You cast does not included Naomi Watts.";
+  }   
+  else if (trueButton1.checked && falseButton2.checked && falseButton3.checked) {
+    answerTextConnect.innerHTML = "Wrong - The highest grosing movie filmed in Seattle is The Ring. <br> Wrong - The Ring was released in 2002. <br> Correct - 10 Things I Hate About You cast does not included Naomi Watts.";
+  }
+  else if (falseButton1.checked && trueButton2.checked && trueButton3.checked) {
+    answerTextConnect.innerHTML = "Correct - The highest grosing movie filmed in Seattle is The Ring. <br> Correct - The Ring was released in 2002. <br> Wrong - 10 Things I Hate About You cast does not included Naomi Watts.";
+  }
+  else if (trueButton1.checked && falseButton2.checked && trueButton3.checked) {
+    answerTextConnect.innerHTML = "Wrong - The highest grosing movie filmed in Seattle is The Ring. <br> Wrong - The Ring was released in 2002. <br> Wrong - 10 Things I Hate About You cast does not included Naomi Watts.";
+  }
+  else if (falseButton1.checked && trueButton2.checked && falseButton3.checked) {
+    answerTextConnect.innerHTML = "Correct - The highest grosing movie filmed in Seattle is The Ring. <br> Correct - The Ring was released in 2002. <br> Correct - 10 Things I Hate About You cast does not included Naomi Watts.";
+  }
+  else if (trueButton1.checked) {
+    answerTextConnect.innerHTML = "Please answer both questions for quiz results.";
+  }
+  else if (falseButton1.checked) {
+    answerTextConnect.innerHTML = "Please answer both questions for quiz results.";
+  }
+	else if (trueButton2.checked) {
+    answerTextConnect.innerHTML = "Please answer both questions for quiz results.";
+  }    
+  else if (falseButton2.checked) {
+    answerTextConnect.innerHTML = "Please answer both questions for quiz results.";
+  }
+  else if (trueButton3.checked) {
+    answerTextConnect.innerHTML = "Please answer both questions for quiz results.";
+  }    
+  else if (falseButton3.checked) {
+    answerTextConnect.innerHTML = "Please answer both questions for quiz results.";
+  }
+  else {
+    answerTextConnect.innerHTML = "Please answer both questions for quiz results.";
+  }
 }
