2f95db61ccbfbf2d0a669f7d8c45b3122e9161b1
SeaFilmz <danielschmidt18@gmail.com>
2020-07-18 15:40:05 -0700

    Updated SeaFilmz Quiz JavaScript
    The answers for each question were put into variables which then were used in the else if statements. This reduced the amount of characters my quiz code used.

--- sfmain.js
+++ sfmain.js
@@ -24,33 +24,39 @@ const falseButton2 = document.querySelector(".falseAnswer2");
 const trueButton3 = document.querySelector(".trueAnswer3");
 const falseButton3 = document.querySelector(".falseAnswer3");
 const answerTextConnect = document.querySelector("#answerText");
+const q1Wrong = "Wrong - The highest grosing movie filmed in Seattle is The Ring.";
+const q1Correct = "Correct - The highest grosing movie filmed in Seattle is The Ring.";
+const q2Wrong = "Wrong - The Ring was released in 2002.";
+const q2Correct = "Correct - The Ring was released in 2002.";
+const q3Wrong = "Wrong - 10 Things I Hate About You cast does not included Naomi Watts.";
+const q3Correct = "Correct - 10 Things I Hate About You cast does not included Naomi Watts.";
 
 function quizButton() {
   if ((!trueButton1.checked && !falseButton1.checked) || (!trueButton2.checked && !falseButton2.checked) || (!trueButton3.checked && !falseButton3.checked)) {
     answerTextConnect.innerHTML = "Please answer all questions for quiz results.";
   }
   else if (trueButton1.checked && trueButton2.checked && trueButton3.checked) {
-    answerTextConnect.innerHTML = "Wrong - The highest grosing movie filmed in Seattle is The Ring. <br> Correct - The Ring was released in 2002. <br> Wrong - 10 Things I Hate About You cast does not included Naomi Watts.";
+    answerTextConnect.innerHTML = q1Wrong + "<br>" + q2Correct +"<br>" + q3Wrong;
   }
   else if (falseButton1.checked && falseButton2.checked && falseButton3.checked) {
-    answerTextConnect.innerHTML = "Correct - The highest grosing movie filmed in Seattle is The Ring. <br> Wrong - The Ring was released in 2002. <br> Correct - 10 Things I Hate About You cast does not included Naomi Watts.";
+    answerTextConnect.innerHTML = q1Correct + "<br>" + q2Wrong + "<br>" + q3Correct;
   }
   else if (trueButton1.checked && trueButton2.checked && falseButton3.checked) {
-    answerTextConnect.innerHTML = "Wrong - The highest grosing movie filmed in Seattle is The Ring. <br> Correct - The Ring was released in 2002. <br> Correct - 10 Things I Hate About You cast does not included Naomi Watts.";
+    answerTextConnect.innerHTML = q1Wrong + "<br>" + q2Correct + "<br>" + q3Correct;
   }
-  else if (falseButton1.checked && falseButton2.checked && trueButton3.checked) {
-    answerTextConnect.innerHTML = "Correct - The highest grosing movie filmed in Seattle is The Ring. <br> Wrong - The Ring was released in 2002. <br> Wrong - 10 Things I Hate About You cast does not included Naomi Watts.";
+   else if (falseButton1.checked && falseButton2.checked && trueButton3.checked) {
+    answerTextConnect.innerHTML = q1Correct + "<br>" + q2Wrong + "<br>" + q3Wrong;
   }   
   else if (trueButton1.checked && falseButton2.checked && falseButton3.checked) {
-    answerTextConnect.innerHTML = "Wrong - The highest grosing movie filmed in Seattle is The Ring. <br> Wrong - The Ring was released in 2002. <br> Correct - 10 Things I Hate About You cast does not included Naomi Watts.";
+    answerTextConnect.innerHTML = q1Wrong + "<br>" + q2Wrong + "<br>" + q3Correct;
   }
   else if (falseButton1.checked && trueButton2.checked && trueButton3.checked) {
-    answerTextConnect.innerHTML = "Correct - The highest grosing movie filmed in Seattle is The Ring. <br> Correct - The Ring was released in 2002. <br> Wrong - 10 Things I Hate About You cast does not included Naomi Watts.";
+    answerTextConnect.innerHTML = q1Correct + "<br>" + q2Correct + "<br>" + q3Wrong;
   }
   else if (trueButton1.checked && falseButton2.checked && trueButton3.checked) {
-    answerTextConnect.innerHTML = "Wrong - The highest grosing movie filmed in Seattle is The Ring. <br> Wrong - The Ring was released in 2002. <br> Wrong - 10 Things I Hate About You cast does not included Naomi Watts.";
+    answerTextConnect.innerHTML = q1Wrong + "<br>" +  q2Wrong + "<br>" + q3Wrong;
   }
   else if (falseButton1.checked && trueButton2.checked && falseButton3.checked) {
-    answerTextConnect.innerHTML = "Correct - The highest grosing movie filmed in Seattle is The Ring. <br> Correct - The Ring was released in 2002. <br> Correct - 10 Things I Hate About You cast does not included Naomi Watts.";
+    answerTextConnect.innerHTML = q1Correct + "<br>" + q2Correct + "<br>" + q3Correct;
   }
 }
