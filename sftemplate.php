26ae7543efe5873ba2af4cdc88220b3eb1dc2f15
SeaFilmz <danielschmidt18@gmail.com>
2020-07-30 10:19:02 -0700

    Expanded PHP Website Template
    The PHP website template has now been expanded to work with the header.

--- sftemplate.php
+++ sftemplate.php
@@ -1,4 +1,4 @@
-<?php require_once("db_connection.php"); ?>
+<?php require_once("db_connectiontest.php"); ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,10 +6,16 @@
     <title><?php echo $title; ?></title>
     <meta name="description" content="<?php echo $mDesc; ?>">
     <meta charset="utf-8">
-    <link type="text/css" rel="stylesheet" href="sfcss.css" />
+    <link type="text/css" rel="stylesheet" href="newsfcssmain.css" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="sfmain.js" defer></script>
 
     <!--Connection to Google Analytics.-->
     <?php include "googleanalytics_connection.php"; ?>
-</head>
+    
+  </head>
+
+  <body class=<?php echo $body; ?>>
+
+    <!--link to header-->
+<?php require_once("sfheader.php"); ?>
