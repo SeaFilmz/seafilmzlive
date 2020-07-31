a4ac396c6e2b9fe72e3d710634da012e1ce77123
SeaFilmz <danielschmidt18@gmail.com>
2020-07-31 00:10:35 -0700

    Updated Head Home Page
    Removed part of the head section and linked to a new seperate file with that part of the head section to make this webpage more dynamic.

--- index.php
+++ index.php
@@ -3,16 +3,12 @@
 <html lang="en">
 
   <head>
-    <!--Connection to Google Analytics.-->
-    <?php include "googleanalytics_connection.php"; ?>
-
-    <meta charset="utf-8">
+    <title>Grassroots (2012) - SeaFilmz</title>
     <meta name="description" content="SeaFilmz is your Seattle media connection with a focus on film.">
-    <title>SeaFilmz - Greater Seattle Film, Media, Data</title>
-    <link type="text/css" rel="stylesheet" href="sfcss.css" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="sfmain.js" defer></script>
-  </head>
+
+    <!--link to part of my head-->
+<?php require_once "sfhead.php"; ?>
+
 
   <body class="MainPage">
     <!--link to header-->
