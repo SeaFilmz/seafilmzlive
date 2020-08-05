9d163959bdb818af43e348bc3a07b308e802202a
SeaFilmz <danielschmidt18@gmail.com>
2020-08-04 22:37:22 -0700

    Updated Links on Home Page
    On the home page links were updated by dropping the file extension because the website has been updated to not need the file extension to go to other pages on this website.

--- index.php
+++ index.php
@@ -9,63 +9,65 @@
 
         <?php
             // 4. Release returned data
-            mysqli_free_result($result);
+            //mysqli_free_result($result);
         ?>
 
-    <div class="MainPageContent">
-      <p class="MainPageAbout">SeaFilmz is a Seattle media and data tool with a focus on movies.</p>
+    <main class="HomePageContent">
 
-      <p class="SeattleMoviesMain"><a href="sfseattlemovies.php">Movies Filmed in Seattle by Title</a></p>
+      <p class="HomePageAbout">SeaFilmz is a Seattle media and data tool with a focus on movies.</p>
 
-      <p class="SeattleMoviesMain"><a href="sfseattlemoviesyear.php">Movies Filmed in Seattle by Year Released</a></p>
+      <p class="SeattleMoviesMain"><a href="sfseattlemovies">Movies Filmed in Seattle by Title</a></p>
 
-      <p class="SeattleMoviesMain"><a href="sfseattlemoviesruntime.php">Movies Filmed in Seattle by Runtime</a></p>
+      <p class="SeattleMoviesMain"><a href="sfseattlemoviesyear">Movies Filmed in Seattle by Year Released</a></p>
 
-      <p class="SeattleMoviesMain"><a href="sfseattlemoviesgross.php">Movies Filmed in Seattle by Total Worldwide Gross</a></p>
+      <p class="SeattleMoviesMain"><a href="sfseattlemoviesruntime">Movies Filmed in Seattle by Runtime</a></p>
 
-      <p class="SeattleActorsMain"><a href="sfseattleactors.php">Actors Born in Seattle by First Name</a></p>
+      <p class="SeattleMoviesMain"><a href="sfseattlemoviesgross">Movies Filmed in Seattle by Total Worldwide Gross</a></p>
 
-      <p class="SeattleActorsMain"><a href="sfseattleactorsbirthdate.php">Actors Born in Seattle by Birthdate</a></p>
+      <p class="SeattleActorsMain"><a href="sfseattleactors">Actors Born in Seattle by First Name</a></p>
 
-      <p class="SeattleMusiciansMain"><a href="sfseattlemusicians.php">Musicians Born in Seattle</a></p>
+      <p class="SeattleActorsMain"><a href="sfseattleactorsbirthdate">Actors Born in Seattle by Birthdate</a></p>
 
-      <p class="SeattleAthletesMain"><a href="sfseattleathletes.php">Athletes Born in Seattle</a></p>
+      <p class="SeattleMusiciansMain"><a href="sfseattlemusicians">Musicians Born in Seattle</a></p>
 
-      <p class="SeattleFunFactsMain"><a href="seattlefunfacts.php">Seattle Fun Facts</a></p>
+      <p class="SeattleAthletesMain"><a href="sfseattleathletes">Athletes Born in Seattle</a></p>
 
-      <p class="SeattleBorderCitiesMain"><a href="washingtoncities.php">Other Washington State Cities</a></p>
-    </div>
+      <p class="SeattleFunFactsMain"><a href="seattle">Seattle Fun Facts</a></p>
 
-    <div class="sfQuizBackground">
+      <p class="SeattleBorderCitiesMain"><a href="washingtoncities">Other Washington State Cities</a></p>
+    
 
-      <h2 class="sfQuizHeader">SeaFilmz Quiz</h2>
+      <div class="sfQuizBackground">
 
-      <form class="sfQuiz">
+        <h2 class="sfQuizHeader">SeaFilmz Quiz</h2>
 
-        <div class="sfQText"><p class="sfQ"><b>Sleepless in Seattle is the highest grossing movie flimed in Seattle.</b></p></div>
+        <form class="sfQuiz">
 
-          <input type="radio" name="answer1" value="true" class="trueAnswer1"> True <br>
-          <input type="radio" name="answer1" value="false" class="falseAnswer1"> False <br>
+          <div class="sfQText"><p class="sfQ"><b>Sleepless in Seattle is the highest grossing movie flimed in Seattle.</b></p></div>
 
-        <p class="sfQ"><b>The Ring was released in 2002.</b></p> 
+            <input type="radio" name="answer1" value="true" class="trueAnswer1"> True <br>
+            <input type="radio" name="answer1" value="false" class="falseAnswer1"> False <br>
 
-          <input type="radio" name="answer2" value="true" class="trueAnswer2"> True <br>
-          <input type="radio" name="answer2" value="false" class="falseAnswer2"> False <br>
+          <p class="sfQ"><b>The Ring was released in 2002.</b></p> 
 
-        <p class="sfQ"><b>10 Things I Hate About You cast included Naomi Watts.</b></p>
+            <input type="radio" name="answer2" value="true" class="trueAnswer2"> True <br>
+            <input type="radio" name="answer2" value="false" class="falseAnswer2"> False <br>
 
-          <input type="radio" name="answer3" value="true" class="trueAnswer3"> True <br>
-          <input type="radio" name="answer3" value="false" class="falseAnswer3"> False <br>      
+          <p class="sfQ"><b>10 Things I Hate About You cast included Naomi Watts.</b></p>
 
-      </form>
+            <input type="radio" name="answer3" value="true" class="trueAnswer3"> True <br>
+            <input type="radio" name="answer3" value="false" class="falseAnswer3"> False <br>      
 
-      <div class="sfQuizButton">
-        <button onclick="quizButton()" class="sfQuizButton"><b>Answer</b></button>
+        </form>
+
+        <div class="sfQuizButton">
+          <button onclick="quizButton()" class="sfQuizButton"><b>Answer</b></button>
+        </div>
+
+        <p id="answerText" class="answerText"></p>
       </div>
 
-      <p id="answerText" class="answerText"></p>
-
-    </div>
+    </main>
 
     <!--link to footer-->
 <?php require_once("sffooter.php"); ?>
