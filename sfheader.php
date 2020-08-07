37118a9aef4e85bb5c8f32db0cb97e3653ca6821
SeaFilmz <danielschmidt18@gmail.com>
2020-08-06 22:08:57 -0700

    Updated Menu Links in Header
    The menu links indside the header were updated by dropping the file extension because the website has been updated to not need the file extension to go to other pages on this website.

--- sfheader.php
+++ sfheader.php
@@ -1,5 +1,5 @@
     <header id="goToTopLink" class="banner">
-      <h1 id="headerStandFor"><a href="/">SeaFilmz</a></h1>
+      <h1 id="headerStandFor"><a href="\">SeaFilmz</a></h1>
       <p class="solgan">6 Degrees of Seattle</p>
 
       <div class="mobile-menu" onclick="onClickMenu()">
@@ -9,14 +9,14 @@
       </div>
 
       <nav class="MobileNav">
-        <p><a href="sfseattlemovies.php">Movies Filmed in Seattle by Title</a></p>
-        <p><a href="sfseattlemoviesyear.php">Movies Filmed in Seattle by Year Released</a></p>
-        <p><a href="sfseattlemoviesruntime.php">Movies Filmed in Seattle by Runtime</a></p>
-        <p><a href="sfseattlemoviesgross.php">Movies Filmed in Seattle by Total Worldwide Gross</a></p>
-        <p><a href="sfseattleactors.php">Seattle Born Actors by First Name</a></p>
-        <p><a href="sfseattleactorsbirthdate.php">Actors Born in Seattle by Birthdate</a></p>
-        <p><a href="sfseattlemusicians.php">Seattle Born Musicians</a></p>
-        <p><a href="sfseattleathletes.php">Seattle Born Athletes</a></p>
+        <p><a href="sfseattlemovies">Movies Filmed in Seattle by Title</a></p>
+        <p><a href="sfseattlemoviesyear">Movies Filmed in Seattle by Year Released</a></p>
+        <p><a href="sfseattlemoviesruntime">Movies Filmed in Seattle by Runtime</a></p>
+        <p><a href="sfseattlemoviesgross">Movies Filmed in Seattle by Total Worldwide Gross</a></p>
+        <p><a href="sfseattleactors">Seattle Born Actors by First Name</a></p>
+        <p><a href="sfseattleactorsbirthdate">Actors Born in Seattle by Birthdate</a></p>
+        <p><a href="sfseattlemusicians">Seattle Born Musicians</a></p>
+        <p><a href="sfseattleathletes">Seattle Born Athletes</a></p>
         <p><a href="seattle">Seattle Facts</a></p>
         <p><a href="washingtoncities">Cities Near Seattle</a></p>
       </nav>
@@ -25,18 +25,18 @@
         <div class="dropdown">
           <span class="NavMove">Movies Filmed in Seattle</span><div class="upsideDownTriangle">&#9660;</div>
           <div class="dropdown-content">
-            <p><a href="sfseattlemovies.php">by Title</a></p>
-            <p><a href="sfseattlemoviesyear.php">by Year Released</a></p>
-            <p><a href="sfseattlemoviesruntime.php">by Runtime</a></p>
-            <p><a href="sfseattlemoviesgross.php">by Total Worldwide Gross</a></p>
+            <p><a href="sfseattlemovies">by Title</a></p>
+            <p><a href="sfseattlemoviesyear">by Year Released</a></p>
+            <p><a href="sfseattlemoviesruntime">by Runtime</a></p>
+            <p><a href="sfseattlemoviesgross">by Total Worldwide Gross</a></p>
           </div>
         </div>
         <div class="dropdown">
           <span class="NavMove">Seattle Born</span><div class="upsideDownTriangle">&#9660;</div>
           <div class="dropdown-content">
-            <p><a href="sfseattleactors.php">Actors</a></p>
-            <p><a href="sfseattlemusicians.php">Musicians</a></p>
-            <p><a href="sfseattleathletes.php">Athletes</a></p>
+            <p><a href="sfseattleactors">Actors</a></p>
+            <p><a href="sfseattlemusicians">Musicians</a></p>
+            <p><a href="sfseattleathletes">Athletes</a></p>
           </div>
         </div>
         <div class="dropdown">
@@ -51,7 +51,7 @@
             <p><a href="mercerisland">Mercer Island</a></p>
             <p><a href="medina">Medina</a></p>
             <p><a href="bellevue">Bellevue</a></p>
-            <p><a href="renton">Renton</a></p>
+            <p><a href="renton">Renton</a></p>            
           </div>
         </div>
       </nav>
