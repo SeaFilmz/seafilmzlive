bec8305aa61c734f5475f20dc8c46d169d4feb9b
SeaFilmz <danielschmidt18@gmail.com>
2020-10-22 13:28:29 -0700

    Updated PHP Website Template with Footer
    Put the header part of my website template into a reusable function. Added the footer to my website template with a resuable function.

--- sftemplate.php
+++ sftemplate.php
@@ -1,21 +1,43 @@
-<?php require_once("db_connection.php"); ?>
-<!DOCTYPE html>
-<html lang="en">
+<?php
+  function headerTemp() {
+?>   
+    <?php require_once("db_connection.php"); ?>
+    <!DOCTYPE html>
+    <html lang="en">
 
-  <head>
-    <title><?php echo $title; ?></title>
-    <meta name="description" content="<?php echo $mDesc; ?>">
-    <meta charset="utf-8">
-    <link type="text/css" rel="stylesheet" href="sfcssmain2.css" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="sfmain.js" defer></script>
+      <head>
+        <title><?php global $title; echo $title; ?></title>
+        <meta name="description" content="<?php echo $mDesc; ?>">
+        <meta charset="utf-8">
+        <link type="text/css" rel="stylesheet" href="sfcssmain2.css" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="sfmain.js" defer></script>
 
-    <!--Connection to Google Analytics.-->
-    <?php include "googleanalytics_connection.php"; ?>
-    
-  </head>
+        <!--Connection to Google Analytics.-->
+        <?php include "googleanalytics_connection.php"; ?>
+        
+      </head>
 
-  <body class=<?php echo $body; ?>>
+      <body class=<?php global $body; echo $body; ?>>
 
-    <!--link to header-->
-<?php require_once("sfheader.php"); ?>
+        <!--link to header-->
+    <?php require_once("sfheader.php"); ?>
+<?php
+  }
+
+  function footer() {
+?>     
+    <footer>
+      <nav class="NavFooter">
+          <p class="NavFooterMobile"><a href="#goToTopLink">Go to Top</a></p>
+
+          <p class="NavFooterMobile"><a href="sfabout">About</a></p>
+
+          <p class="NavFooterMobile"><a href="sfcontact">Contact</a></p>
+
+          <p class="NavFooterMobile"><a href="sfservices">Services</a></p>
+      </nav>
+    </footer>
+<?php
+  }
+?>
