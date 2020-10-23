60ce5c764deae46359ce1e4dc5618890b1b2b75f
SeaFilmz <danielschmidt18@gmail.com>
2020-10-22 21:31:18 -0700

    Portland, Oregon City Fact Page
    Developed a city fact page for Portland, Oregon.

---
+++ portlandor.php
@@ -0,0 +1,104 @@
+<!--link to the start of a seafilmz general webpage template-->
+<?php
+  $title = "Portland, Oregon - SeaFilmz"; 
+  $mDesc = "Fact page about the city of Portland, Oregon.";
+  $body = "MainBody";
+  require_once "sftemplate.php";
+?>
+
+
+    <h2 class="CityPageHeader">Portland, Oregon</h2>
+
+    <div class="CityPageContent">
+      <p><b>Officially Became a City:</b> February 8, 1851</p>
+      <p><b>Country:</b> <a href="https://www.usa.gov/">United States of America</a></p>
+      <p><b>State:</b> <a href="https://www.oregon.gov/Pages/index.aspx">Oregon</a></p>
+      <p><b>County:</b> 
+        <a href="https://multco.us/">Multnomah</a>,
+        <a href="https://www.clackamas.us/">Clackamas</a>,
+        <a href="https://www.co.washington.or.us/">Washington</a>
+      </p>
+      <p><b>City Website:</b> <a href="https://www.portland.gov/">portland.gov</a></p>
+      <p><b>Movie Filmed in Portland, Oregon:</b> <a href="pdfportlandormovies">List of Movies Filmed in Portland, Oregon</a></p>
+      <p><b>Movie Theaters in Portland, Oregon:</b>
+        <a href="https://www.academytheaterpdx.com/">Academy Theater</a>,
+        <a href="https://www.wunderlandgames.com/avalontheatre.asp">Avalon Theatre & Wunderland</a>,
+        <a href="https://www.mcmenamins.com/bagdad-theater-pub">Bagdad Theater & Pub</a>,
+        <a href="https://www.cinemark.com/theatres/or-portland/century-16-eastport-plaza">Century 16 Eastport Plaza</a>,
+        <a href="https://www.cinema21.com/">Cinema 21</a>,
+        <a href="https://thecinemagictheater.com/">CineMagic</a>,
+        <a href="https://cstpdx.com/">The Clinton Street Theater</a>,
+        <a href="https://omsi.edu/theater?utm_source=Google%20Maps%20-%20Locl.io&utm_medium=Website%20Button&utm_campaign=Locl.io%202019">The Empirical Theater</a>,
+        <a href="http://www.5thavecinema.com/">5th Avenue Cinema</a>,
+        <a href="https://hollywoodtheatre.org/">Hollywood Theatre</a>,
+        <a href="https://jubitz.com/dining-entertainment/jubitz-cinema/">Jubitz Cinema</a>,
+        <a href="https://www.mcmenamins.com/kennedy-school/kennedy-school-theater">Kennedy School Theater</a>,
+        <a href="https://www.laurelhursttheater.com/">Laurelhurst Theater and Pub</a>,
+        <a href="http://pdx.livingroomtheaters.com/index.cfm">Living Room Theaters</a>,
+        <a href="https://www.mcmenamins.com/mission-theater">Mission Theater</a>,
+        <a href="https://morelandtheater.com/">Moreland Theatre</a>,
+        <a href="https://www.mcmenamins.com/st-johns-theater-pub">St. Johns Theater and Pub</a>,
+        <a href="https://stjohnscinema.com/">St. Johns Twin Cinema and Pub</a>,
+        <a href="https://www.regmovies.com/theatres/regal-division-street/0851#/buy-tickets-by-cinema?in-cinema=0851&view-mode=list">Regal Division Street</a>,
+        <a href="https://www.regmovies.com/theatres/regal-fox-tower/0633#/buy-tickets-by-cinema?in-cinema=0633&view-mode=list">Regal Fox Tower</a>,
+        <a href="https://www.regmovies.com/theatres/regal-lloyd-center-imax/0807#/buy-tickets-by-cinema?in-cinema=0807&view-mode=list">Regal Lloyd Center & IMAX</a>,
+        <a href="http://www.rosewaytheater.com/">Roseway Theater</a>
+      </p>
+      
+      <p><b>Professional Sports Teams:</b>
+        <a href="https://www.nba.com/blazers/">Portland Trail Blazers</a> (men's basketball),
+        <a href="https://www.timbers.com/">Portland Timbers</a> (men's soccer),
+        <a href="https://www.nwslsoccer.com/teams/portland-thorns">Portland Thorns FC</a> (women's soccer)
+      </p>
+      <p><b>Universities or Colleges:</b>
+        <a href="https://www.up.edu/">University of Portland</a>,
+        <a href="https://www.pdx.edu/">Portland State University</a>,
+        <a href="https://www.lclark.edu/">Lewis & Clark College</a>,
+        <a href="https://www.reed.edu/">Reed College</a>,
+        <a href="https://www.multnomah.edu/">Multnomah University</a>,
+        <a href="https://www.warnerpacific.edu/">Warner Pacific University</a>,
+        <a href="https://www.pcc.edu/">Portland Community College</a>
+      </p>
+      <p><b>Board Game Hangout Places:</b>
+        <a href="http://www.theportlandgamestore.com/">The Portland Game Store</a>,
+        <a href="https://www.moxboardinghouse.com/">Mox Boarding House</a>,
+        <a href="https://ggportland.com/">Guardian Games</a>,
+        <a href="https://redcastlegames.com/">Red Castle Games</a>,
+        <a href="https://cloudcapgames.com/">Cloud Cap Games</a>
+      </p>
+      <p><b>Bowling Alleys:</b> 
+        <a href="https://www.thegrandcentralbowl.com/">Grand Central Bowling Lounge</a>, 
+        <a href="https://punchbowlsocial.com/location/portland">Punch Bowl Social</a>,
+        <a href="https://mykingpins.com/">KingPins Family Entertainment Center</a>
+      </p>
+      <p><b>Golf Courses:</b>
+        <a href="https://www.heronlakesgolf.com/">Heron Lakes Golf Club</a>,
+        <a href="https://www.cecc.com/">Columbia Edgewater Country Club</a>,
+        <a href="https://www.riversidegcc.com/">Riverside Golf & Country Club</a>,
+        <a href="http://broadmoor-1931.com/broadmoor-1931/WELCOME.html">Broadmoor Golf Course</a>,
+        <a href="https://www.colwoodgolf.com/">Colwood Golf Center</a>,
+        <a href="https://www.rockcreekcountryclub.com/">Rock Creek Country Club - Portland</a>,
+        <a href="https://www.claremontgolfclub.com/">Claremont Golf Club</a>,
+        <a href="https://www.rosecitygc.com/">Rose City Golf Course</a>
+        <a href="https://www.golfglendoveer.com/">Glendoveer Golf & Tennis</a>,
+        <a href="https://www.eastmorelandgolfcourse.com/">Eastmoreland Golf Course</a>
+        <a href="https://www.waverley.cc/">Waverley Country Club</a>
+      </p>
+    </div>
+
+        <?php
+            // 4. Release returned data
+            //mysqli_free_result($result);
+        ?>
+
+    <!--link to footer-->
+<?php require_once "sftemplate.php"; ?>
+
+  </body>
+
+</html>
+
+<?php
+    // 5. Close database connection
+    mysqli_close($connection);
+?>
