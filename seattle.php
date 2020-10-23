90238a00049db884f4e5b9a257e72c830e70a3db
SeaFilmz <danielschmidt18@gmail.com>
2020-10-22 21:48:36 -0700

    Updated Seattle Facts Page
    Updated the order of the Seattle city facts page. Added a section for golf couse in this city.

--- seattle.php
+++ seattle.php
@@ -10,13 +10,13 @@
     <h2 class="SeattleFFHeader">Seattle Fun Facts</h2>
 
     <div class="SeattleFFContent">
-      <p><b>Officially Became a City:</b> December 2, 1869</p>	      
-      <p><b>County:</b> <a href="http://www.kingcounty.gov/">King</a></p>
-      <p><b>State:</b> <a href="http://access.wa.gov/">Washington</a></p>
+      <p><b>Officially Became a City:</b> December 2, 1869</p>
       <p><b>Country:</b> <a href="https://www.usa.gov/">United States of America</a></p>
-      <p><b>Website:</b> <a href="http://www.seattle.gov/">www.seattle.gov</a></p>
-      <p><b>Movies Filmed in Seattle:</b> <a href="sfseattlemovies.php">List of Movies Filmed in Seattle</a>
-      <p><b>Movie Theaters in Seattle:</b> <a href="seattlemovietheaters.php">List of Movie Theaters in Seattle</a></p>
+      <p><b>State:</b> <a href="http://access.wa.gov/">Washington</a></p>
+      <p><b>County:</b> <a href="http://www.kingcounty.gov/">King</a></p>
+      <p><b>City Website:</b> <a href="http://www.seattle.gov/">seattle.gov</a></p>
+      <p><b>Movies Filmed in Seattle:</b> <a href="sfseattlemovies">List of Movies Filmed in Seattle</a>
+      <p><b>Movie Theaters in Seattle:</b> <a href="seattlemovietheaters">List of Movie Theaters in Seattle</a></p>
       <p><b>Professional Sports Teams:</b>
         <a href="http://www.seahawks.com/">Seattle Seahawks</a> (men's football),
         <a href="http://storm.wnba.com/">Seattle Storm</a> (women's basketball),
@@ -33,8 +33,8 @@
         <a href="http://www.seattlecentral.org/">Seattle Central College</a>,
         <a href="http://www.southseattle.edu/">South Seattle College</a>
       </p>
-      <p><b>Libraries in Seattle:</b> <a href="https://www.spl.org/hours-and-locations">List of Libraries in Seattle</a></p>
-      <p><b>Board Game Hangout Places in Seattle:</b>
+      <p><b>Libraries:</b> <a href="https://www.spl.org/hours-and-locations">List of Libraries in Seattle</a></p>
+      <p><b>Board Game Hangout Places:</b>
         <a href="https://www.moxboardinghouse.com/">Mox Boarding House</a>,
         <a href="http://www.bluehighwaygames.com/">Blue Highway Games</a>,
         <a href="http://www.gammaraygamestore.com/">Gamma Ray Games</a>,
@@ -42,6 +42,16 @@
         <a href="http://meeplesgames.com/">Meeples Games</a>
       </p>
       <p><b>Bowling Alley:</b> <a href="https://www.garagebilliards.com/">Garage</a>, <a href="http://www.wsbowl.com/">West Seattle Bowl</a></p>
+      <p><b>Golf Courses:</b>
+        <a href="http://premiergc.com/-jackson-park-golf-course">Jackson Park Golf Course</a>,
+        <a href="http://www.greenlakegolfcourse.com/">Green Lake Golf Course</a>,
+        <a href="http://www.premiergc.com/-interbay-golf-center">The Links at Interbay</a>,
+        <a href="https://broadmoorgolfclub.com/">Broadmoor Golf Club</a>,
+        <a href="http://premiergc.com/-jefferson-park-golf-course">Jefferson Park Golf Course</a>,
+        <a href="http://premiergc.com/-west-seattle-golf-course">West Seattle Golf Course</a>,
+        <a href="https://www.rainiergolfcc.com/">Rainier Golf and Country Club</a>,
+        <a href="https://www.glenacresgolf.com/">Glen Acres Golf & Country Club</a>
+      </p>
     </div>
 
 		<div class="SeattleFFImage">
@@ -59,7 +69,7 @@
     </div>
 
     <!--link to footer-->
-<?php require_once("sffooter.php"); ?>
+<?php require_once "sftemplate.php"; ?>
 
   </body>
 
