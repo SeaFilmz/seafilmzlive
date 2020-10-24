b38c748126ea3dabc08d229795c18f9385f3edce
SeaFilmz <danielschmidt18@gmail.com>
2020-10-23 21:20:12 -0700

    Updated Shoreline Facts Page
    Updated the order of the Shoreline city facts page. Added a section for golf couse in this city.

--- shoreline.php
+++ shoreline.php
@@ -10,11 +10,11 @@
     <h2 class="CityPageHeader">City of Shoreline (Northwest Bordering City of Seattle)</h2>
 
 		<div class="CityPageContent">
-		  <p><b>Officially Became a City:</b> August 31, 1995</p>
-		  <p><b>County:</b> <a href="http://www.kingcounty.gov/">King</a></p>
-		  <p><b>State:</b> <a href="http://access.wa.gov/">Washington</a></p>
+      <p><b>Officially Became a City:</b> August 31, 1995</p>
 		  <p><b>Country:</b> <a href="https://www.usa.gov/">United States of America</a></p>
-		  <p><b>Website:</b> <a href="http://www.shorelinewa.gov/">www.shorelinewa.gov</a></p>
+		  <p><b>State:</b> <a href="http://access.wa.gov/">Washington</a></p>
+		  <p><b>County:</b> <a href="http://www.kingcounty.gov/">King</a></p>
+		  <p><b>City Website:</b> <a href="http://www.shorelinewa.gov/">shorelinewa.gov</a></p>
       <p><b>College:</b> <a href="http://www.shoreline.edu/">Shoreline Community College</a></p>
       <p><b>Libraries:</b>
         <a href="https://kcls.org/locations/1535/">Shoreline Library</a>,
@@ -23,6 +23,7 @@
 		  <p><b>Movies Filmed in Shoreline:</b> <a href="sflaggies.php">Laggies</a></p>
       <p><b>Movie Theaters in Shoreline:</b> <a href="https://www.landmarktheatres.com/seattle/crest-cinema-center">Landmark Crest Cinema Center</a></p>
       <p><b>Bowling Alley:</b> <a href="http://spinalleybowling.com">Spin Alley</a></p>
+      <p><b>Golf Course:</b> <a href="https://www.seattlegolfclub.com/">Seattle Golf Club</a></p>
 		</div>
 
         <?php     
@@ -31,7 +32,7 @@
         ?>
 
     <!--link to footer-->
-<?php require_once("sffooter.php"); ?>
+<?php require_once "sffooter.php"; ?>
 
 </html>
 
