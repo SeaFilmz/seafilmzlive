93b9490a2a378b2f8a9e1139594a8619b3897dbb
SeaFilmz <danielschmidt18@gmail.com>
2020-10-23 21:02:44 -0700

    Updated Bellevue Facts Page
    Updated the order of the Bellevue city facts page. Added a section for golf couse in this city.

--- bellevue.php
+++ bellevue.php
@@ -10,11 +10,11 @@
     <h2 class="CityPageHeader">Bellevue (Eastside of Lake Washington City)</h2>
 
 		<div class="CityPageContent">
-		  <p><b>Officially Became a City:</b> 1953</p>
-		  <p><b>County:</b> <a href="http://www.kingcounty.gov/">King</a></p>
-		  <p><b>State:</b> <a href="http://access.wa.gov/">Washington</a></p>
-		  <p><b>Country:</b> <a href="https://www.usa.gov/">United States of America</a></p>
-		  <p><b>Website:</b> <a href="https://bellevuewa.gov/">bellevuewa.gov</a></p>
+      <p><b>Officially Became a City:</b> 1953</p>
+      <p><b>Country:</b> <a href="https://www.usa.gov/">United States of America</a></p>
+ 		  <p><b>State:</b> <a href="http://access.wa.gov/">Washington</a></p>     
+      <p><b>County:</b> <a href="http://www.kingcounty.gov/">King</a></p>
+		  <p><b>City Website:</b> <a href="https://bellevuewa.gov/">bellevuewa.gov</a></p>
       <p><b>College:</b> <a href="https://www.bellevuecollege.edu/">Bellevue College</a>
       <p><b>Libraries:</b>
         <a href="https://kcls.org/locations/1492/">Bellevue Library</a>,
@@ -28,11 +28,17 @@
         <a href="https://www.regmovies.com/theatres/regal-crossroads-bellevue/0905#/">Regal Crossroads - Bellevue</a>,
         <a href="https://www.amctheatres.com/movie-theatres/seattle-tacoma/amc-factoria-8">AMC Factoria 8</a>
       </p>
-      <p><b>Board Game Hangout Places in Bellevue:</b>
+      <p><b>Board Game Hangout Places:</b>
         <a href="https://www.moxboardinghouse.com/">Mox Boarding House</a>,
         <a href="http://www.unclesgames.com/">Uncle's Games</a>
       </p>
       <p><b>Bowling Alleys:</b> <a href="https://www.luckystrikesocial.com/locations/bellevue/">Lucky Strike</a></p>
+      <p><b>Golf Courses:</b>
+        <a href="http://bellevuepgc.com/">Bellevue Golf Course</a>,
+        <a href="https://www.glendalecc.com/">Glendale Country Club</a>,
+        <a href="http://www.bellevuepgc.com/-crossroads-par-3-course">Bellevue Crossroads Par 3 Golf</a>,
+        <a href="https://www.tamoshanter.net/">Tam o'Shanter Golf and Country Club</a>
+      </p>  
 		</div>
 
         <?php
@@ -41,7 +47,7 @@
         ?>
 
     <!--link to footer-->
-<?php require_once("sffooter.php"); ?>
+<?php require_once "sffooter.php"; ?>
 
   </body>
 
