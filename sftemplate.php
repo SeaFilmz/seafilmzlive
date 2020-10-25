96ce4ad6852ae01c7ae5105b76863d7858726921
SeaFilmz <danielschmidt18@gmail.com>
2020-10-25 16:21:40 -0700

    Updated PHP Website Template
    Updated PHP website template requires to use single quotes rather than double quotes for better performance. Also reduced some code by moving the global variable declares to right after I stareted the function.

--- sftemplate.php
+++ sftemplate.php
@@ -1,13 +1,13 @@
 <?php
   function headerTemp() {
-   
-    require_once("db_connection.php"); 
+    global $title, $mDesc, $body;
+    require_once 'db_connection.php'; 
 ?>
     <!DOCTYPE html>
     <html lang="en">
 
       <head>
-        <title><?php global $title; echo $title; ?></title>
+        <title><?php echo $title; ?></title>
         <meta name="description" content="<?php echo $mDesc; ?>">
         <meta charset="utf-8">
         <link type="text/css" rel="stylesheet" href="sfcssmain2.css" />
@@ -15,15 +15,15 @@
         <script src="sfmain.js" defer></script>
 
         <!--Connection to Google Analytics.-->
-        <?php include "googleanalytics_connection.php"; ?>
+        <?php include 'googleanalytics_connection.php'; ?>
         
       </head>
 
-      <body class=<?php global $body; echo $body; ?>>
+      <body class=<?php echo $body; ?>>
 
         <!--link to header-->
 <?php 
-        require_once("sfheader.php");
+        require_once 'sfheader.php';
 
   }
 
