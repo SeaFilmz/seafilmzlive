c42102e745c2492104e7d8416ec02489b6631485
SeaFilmz <danielschmidt18@gmail.com>
2020-10-24 17:49:58 -0700

    Updated PHP Website Template PHP Tags
    Updated PHP website template to reduce the number of php tags used.

--- sftemplate.php
+++ sftemplate.php
@@ -1,28 +1,30 @@
 <?php
   function headerTemp() {
-?>   
-    <?php require_once 'db_connection.php'; ?>
+   
+    require_once("db_connectiontest.php"); 
+?>
     <!DOCTYPE html>
     <html lang="en">
 
       <head>
         <title><?php global $title; echo $title; ?></title>
-        <meta name="description" content="<?php global $mDesc; echo $mDesc; ?>">
+        <meta name="description" content="<?php echo $mDesc; ?>">
         <meta charset="utf-8">
-        <link type="text/css" rel="stylesheet" href="sfcssmain2.css" />
+        <link type="text/css" rel="stylesheet" href="newsfcssmain.css" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <script src="sfmain.js" defer></script>
 
         <!--Connection to Google Analytics.-->
-        <?php include 'googleanalytics_connection.php'; ?>
+        <?php include "googleanalytics_connection.php"; ?>
         
       </head>
 
       <body class=<?php global $body; echo $body; ?>>
 
         <!--link to header-->
-    <?php require_once 'sfheader.php'; ?>
-<?php
+<?php 
+        require_once("sfheader.php");
+
   }
 
   function footer() {
