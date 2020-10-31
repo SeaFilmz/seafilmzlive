148aa9348931c0a40919b89050a324dbaa4b1c29
SeaFilmz <danielschmidt18@gmail.com>
2020-10-30 19:59:06 -0700

    Added Movie Functions PHP
    Added this file to house different functions for the movie data. The 1st function in this file calculates the number of movies in the database for a specific city.

---
+++ moviefunctions.php
@@ -0,0 +1,42 @@
+<?php
+  function cityMoviesCount($city) {
+    global $query, $connection, $result;
+?>
+
+<div class="MCTable">
+        <table class="MovieCountTable">
+          <tr>
+            <th class="MovieCountRowHeader">Total Movies</th>
+
+        <?php
+            // 2. Perform database query
+            $query = "SELECT COUNT(*) ";
+            $query .= "FROM moviesfilminglocation ";
+            $query .= "INNER JOIN movies ";
+            $query .= "ON movies.MovieID = moviesfilminglocation.MovieID ";
+            $query .= "INNER JOIN filminglocations ";
+            $query .= "ON filminglocations.FilmingLocationID = moviesfilminglocation.FilmingLocationID ";
+            $query .= "WHERE City = '$city' ";
+            //Result variable with an error check
+            $result = mysqli_query($connection, $query)
+              or die("Database query failed.");
+
+            // 3. Use returned data (if any)
+            while($movies = mysqli_fetch_assoc($result)) {
+                // output data from each row
+        ?>
+
+            <td class="MovieCountNumber"><?php echo $movies["COUNT(*)"]; ?></td>
+          </tr>
+
+        <?php
+            }
+
+            // 4. Release returned data
+            mysqli_free_result($result);
+        ?>
+
+        </table>
+        </div>
+
+      <?php  } ?>
