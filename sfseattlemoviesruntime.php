2bd432149c7bd75c1e55d219453934ba470ca395
SeaFilmz <danielschmidt18@gmail.com>
2020-11-01 23:00:32 -0800

    Update Total Runtime Seattle Movies Runtime Page
    Update the file name and added a reusable function for the total runtime in movies filmed in Seattle by runtime page file.

--- sfseattlemoviesruntime.php
+++ sfseattlemoviesruntime.php
@@ -50,41 +50,10 @@
         </table>
         </div>
 
-    <div class="MTRTable">
-    <table class="MovieTotalRuntimeTable">
-      <tr class="MoviesContent">
-        <th class="MovieTotalRuntimeRowHeader">Total Seattle Movie Runtime</th>
-
-        <?php
-            // 2. Perform database query
-            $query = "SELECT SUM(RunTime) ";
-            $query .= "FROM moviesfilminglocation ";
-            $query .= "INNER JOIN movies ";
-            $query .= "ON movies.MovieID = moviesfilminglocation.MovieID ";
-            $query .= "INNER JOIN filminglocations ";
-            $query .= "ON filminglocations.FilmingLocationID = moviesfilminglocation.FilmingLocationID ";
-            $query .= "WHERE City = 'Seattle' ";
-            //Result variable with an error check
-            $result = mysqli_query($connection, $query)
-              or die("Database query failed.");
-
-            // 3. Use returned data (if any)
-            while($movies = mysqli_fetch_assoc($result)) {
-                // output data from each row
-        ?>
-
-        <td class="MovieTotalRuntimeNumber"><?php echo number_format($movies["SUM(RunTime)"]); ?> Minutes</td>
-      </tr>
-
-        <?php
-            }
-  
-            // 4. Release returned data
-            mysqli_free_result($result);
-        ?>
-
-    </table>
-    </div>
+<?php 
+  require 'moviefunctions.php';
+  cityRuntimeCount('Seattle');
+?>
 
     <div class="MTRTable">
     <table class="MovieAvgRuntimeTable">
