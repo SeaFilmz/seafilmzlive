fa930b314d779dc08adb55bd49193685537b126c
Daniel Schmidt <61772102+SeaFilmz@users.noreply.github.com>
2024-10-06 12:38:46 -0700

    Remove Washington State Cities Page Link from Main

--- index.php
+++ index.php
@@ -26,8 +26,6 @@
 
       <p class="SeattleFunFactsMain"><a href="seattle">Seattle Fun Facts</a></p>
 
-      <p class="SeattleBorderCitiesMain"><a href="washingtoncities">Other Washington State Cities</a></p>
-
 
       <div class="sfQuizBackground">
 
