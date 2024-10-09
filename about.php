f7637f98a7e641bf02ea406e50e647c8addec497
Daniel Schmidt <61772102+SeaFilmz@users.noreply.github.com>
2024-10-08 23:29:10 -0700

    Move About Description Questions to Each Be on a Different Line

--- about.php
+++ about.php
@@ -10,9 +10,12 @@
 
     <section class="aboutpagecontent">
       <h2 class="AboutHeader">About SeaFilmz</h2>
-
+      
       <div class="AboutDescription">
-        <p>Do you enjoy movies, music, or sports? Do you watch movies or sports? Do you listen music? Do you want to learn about Seattle? If you said yes to any of these four questions then this website is for you.</p>
+        <p>Do you enjoy movies, music, or sports?</p>
+        <p>Do you watch movies or sports?</p>
+        <p>Do you listen music? Do you want to learn about Seattle?</p>
+        <p>If you said yes to any of these four questions then this website is for you.</p>
         <p>This website is SeaFilmz which is a media website about Seattle. SeaFilmz is developed by a person who grew up in the greater Seattle area and is a current resident who is into media and is a movie enthusiast.</p>
       </div>
     </section>
