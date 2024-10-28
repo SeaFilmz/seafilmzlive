4c834e6b0cdbb83a3281cac4fff7b9d39abf9bb6
SeaFilmz <danielschmidt18@gmail.com>
2024-10-27 20:55:22 -0700

    Remove not used nav and footer files

--- sffootermenu.php
+++
@@ -1,5 +0,0 @@
-<li class="NavFooterMobile" style="float:right"><a href="sfcontact.php">Contact</a></li>
-
-          <li class="NavFooterMobile" style="float:right"><a href="sfservices.php">Services</a></li>
-
-          <li class="NavFooterMobile" style="float:right"><a href="sfabout.php">About</a></li>
