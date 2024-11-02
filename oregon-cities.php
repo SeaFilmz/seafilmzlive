7b74700c3d331b8c546d66f6f4df42d1962c2fde
SeaFilmz <danielschmidt18@gmail.com>
2024-11-01 22:33:45 -0700

    Add Oregon State Cities Page
    This webpage has a list of links to different Oregon state city pages on this website.

---
+++ oregon-cities.php
@@ -0,0 +1,41 @@
+<!--link to the start of a seafilmz general webpage template-->
+<?php
+  $title = 'Oregon State Cities - SeaFilmz'; 
+  $mDesc = 'List of important Oregon cities.';
+  $body = 'MainBody';
+  require_once 'sftemplate.php';
+  headerTemp();
+?>
+
+
+		<h2 class="OregonCitiesHeader"><b>Oregon State Cities</b></h2>
+
+    <div class="OregonCitiesContent">
+
+      <p class="OregonCitiesLink"><a href="portland-oregon">Portland</a></p>
+
+      <p class="OregonCitiesLink"><a href="salem-oregon">Salem</a></p>
+
+      <p class="OregonCitiesLink"><a href="eugene-oregon">Eugene</a></p>
+
+      <p class="OregonCitiesLink"><a href="gresham-oregon">Gresham</a></p>
+
+      <p class="OregonCitiesLink"><a href="happy-valley-oregon">Happy Valley</a></p>
+
+      <p class="OregonCitiesLink"><a href="milwaukie-oregon">Milwaukie</a></p>
+
+      <p class="OregonCitiesLink"><a href="lake-oswego-oregon">Lake Oswego</a></p>
+
+      <p class="OregonCitiesLink"><a href="tigard-oregon">Tigard</a></p>
+
+      <p class="OregonCitiesLink"><a href="beaverton-oregon">Beaverton</a></p>
+
+      <p class="OregonCitiesLink"><a href="corvallis-oregon">Corvallis</a></p>      
+
+    </div>
+
+    
+<?php
+  // footer display function
+  footerTemp();
+?>
