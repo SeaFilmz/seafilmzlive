ab430ae5d09c540be3580a974a47c654faeb44d4
SeaFilmz <danielschmidt18@gmail.com>
2024-11-11 23:32:23 -0800

    Update the Portland, Oregon Athletes Count Query
    Update this queries because birth city was moved to a many to many relationship between the peoples and cities tables.

--- portland-oregon-athletes.php
+++ portland-oregon-athletes.php
@@ -64,7 +64,7 @@
 
         <?php
             // 2. Perform database query
-            $query = $newconnection->prepare("SELECT COUNT(*) FROM peoplesjobs INNER JOIN peoples ON peoples.PeopleID = peoplesjobs.PeopleID INNER JOIN jobs ON jobs.JobID = peoplesjobs.JobID WHERE CityTownBorn = ? AND Jobs = ? ");
+            $query = $newconnection->prepare("SELECT COUNT(*) FROM peoplesjobs INNER JOIN peoples ON peoples.PeopleID = peoplesjobs.PeopleID INNER JOIN jobs ON jobs.JobID = peoplesjobs.JobID INNER JOIN peoplebirthcity ON peoples.PeopleID = peoplebirthcity.PeopleID INNER JOIN cities ON cities.CityID = peoplebirthcity.CityID WHERE City = ? AND Jobs = ? ");
 
             $query->bind_param("ss", $city, $job);
             $query->execute();
