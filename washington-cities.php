1f01d9111719028841f68d0e66de359d8a1ddaf2
SeaFilmz <danielschmidt18@gmail.com>
2024-11-30 17:08:44 -0800

    Update Washington State Cities Webpage to Use an Array and For Loop
    This was done to improve the maintainability of the code.

--- washington-cities.php
+++ washington-cities.php
@@ -7,61 +7,44 @@
   headerTemp();
 ?>
 
-
     <h2 class="WashingtonCitiesHeader"><b>Other Washington State Cities</b></h2>
 
     <div class="WashingtonCitiesContent">
 
-      <p class="WashingtonCitiesLink"><a href="seattle">Seattle</a></p>
+      <?php $cities = [
+        "Seattle",
+        "Shoreline",
+        "Lake Forest Park",
+        "Tukwila",
+        "Burien",
+        "SeaTac",
+        "Mercer Island",
+        "Medina",
+        "Bellevue",
+        "Renton",
+        "Kirkland",
+        "Kenmore",
+        "Olympia",
+        "Spokane",
+        "Vancouver",
+        "Pullman",
+        "Bellingham",
+        "Bothell",
+        "Edmonds",
+        "Woodway",
+        "Lynnwood",
+        "Gig Harbor",
+      ];
 
-      <p class="WashingtonCitiesLink"><a href="shoreline-washington">Shoreline</a></p>
-
-      <p class="WashingtonCitiesLink"><a href="lake-forest-park-washington">Lake Forest Park</a></p>
-
-      <p class="WashingtonCitiesLink"><a href="tukwila-washington">Tukwila</a></p>
-
-      <p class="WashingtonCitiesLink"><a href="burien-washington">Burien</a></p>
-
-      <p class="WashingtonCitiesLink"><a href="seatac-washington">SeaTac</a></p>
-
-      <p class="WashingtonCitiesLink"><a href="mercer-island-washington">Mercer Island</a></p>
-
-      <p class="WashingtonCitiesLink"><a href="medina-washington">Medina</a></p>
-
-      <p class="WashingtonCitiesLink"><a href="bellevue-washington">Bellevue</a></p>
-
-      <p class="WashingtonCitiesLink"><a href="renton-washington">Renton</a></p>
-
-      <p class="WashingtonCitiesLink"><a href="kirkland-washington">Kirkland</a></p>
-
-      <p class="WashingtonCitiesLink"><a href="kenmore-washington">Kenmore</a></p>
-
-      <p class="WashingtonCitiesLink"><a href="everett-washington">Everett</a></p>
-
-      <p class="WashingtonCitiesLink"><a href="tacoma-washington">Tacoma</p>
-
-      <p class="WashingtonCitiesLink"><a href="olympia-washington">Olympia</a></p>
-
-      <p class="WashingtonCitiesLink"><a href="spokane-washington">Spokane</p>
-
-      <p class="WashingtonCitiesLink"><a href="vancouver-washington">Vancouver, Washington</a></p>
-
-      <p class="WashingtonCitiesLink"><a href="pullman-washington">Pullman</a></p>
-
-      <p class="WashingtonCitiesLink"><a href="bellingham-washington">Bellingham</a></p>
-
-      <p class="WashingtonCitiesLink"><a href="bothell-washington">Bothell</a></p>
-
-      <p class="WashingtonCitiesLink"><a href="edmonds-washington">Edmonds</a></p>
-
-      <p class="WashingtonCitiesLink"><a href="woodway-washington">Woodway</a></p>
-
-      <p class="WashingtonCitiesLink"><a href="lynnwod-washington">Lynnwood</a></p>
-
-      <p class="WashingtonCitiesLink"><a href="gig-harbor-washington">Gig Harbor</a></p>
+      for ($i = 0; $i < count(cities); $i++) { 
+        if (str_contains($cities, "")) { ?>
+          <p class="WashingtonCitiesLink"><a href="<?= "{strtolower(cities)}-washington"; ?>"><?= cities[$i]; ?></a></p>
+        <?php } else { ?>
+          <p class="WashingtonCitiesLink"><a href="<?= "{str_replace(' ', '-', strtolower(cities)}-washington"; ?>"><?= cities[$i]; ?></a></p>
+        <?php }
+      } ?>
 
     </div>
-
     
 <?php
   // footer display function
