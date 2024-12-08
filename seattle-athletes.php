2fdd74ef2db4a4634cb1394d74ed8224e2b553ff
SeaFilmz <danielschmidt18@gmail.com>
2024-12-08 10:36:35 -0800

    Use cityAthletesCount Function for Seattle and Portland Athletes Pages
    This helps improve the maintainability of website code.

--- seattle-athletes.php
+++ seattle-athletes.php
@@ -57,39 +57,11 @@
     </table>
     </div>
 
-    <div class="ACTable">
-    <table class="AthletesCountTable">
-      <tr class="MoviesContent">
-        <th class="AthletesCountRowHeader">Total Athletes</th>
-
-        <?php
-            // 2. Perform database query
-            $query = $newconnection->prepare("SELECT COUNT(*) FROM peoplesjobs INNER JOIN peoples ON peoples.PeopleID = peoplesjobs.PeopleID INNER JOIN jobs ON jobs.JobID = peoplesjobs.JobID INNER JOIN cities ON cities.CityID = peoples.BirthCityID WHERE City = ? AND Jobs = ? ");
-
-            $query->bind_param("ss", $city, $job);
-            $query->execute();
-            
-            //Result variable with an error check
-            $result = $query->get_result()
-              or die("Database query failed.");
-
-            // 3. Use returned data (if any)
-            while($athletes = mysqli_fetch_assoc($result)) {
-                // output data from each row
-        ?>
-
-        <td class="AthletesCountNumber"><?php echo $athletes["COUNT(*)"]; ?></td>
-      </tr>
-
-        <?php
-            }
-
-            // 4. Release returned data
-            mysqli_free_result($result);
-        ?>
-
-    </table>
-    </div>
+    <!--link to Total Athletes Count-->
+    <?php 
+  require 'queryfunctions/athletefunctions.php';
+  cityAthletesCount('Seattle', 'athlete');
+?>
 
 <?php
   // footer display function
