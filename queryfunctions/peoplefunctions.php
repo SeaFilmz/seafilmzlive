f5a3587bd85a368159169c08d7695b9b5beb0f18
SeaFilmz <danielschmidt18@gmail.com>
2024-12-16 18:37:13 -0800

    Cleanup Whitespace in peoplefunctions File to Improve Code Readability

--- queryfunctions/peoplefunctions.php
+++ queryfunctions/peoplefunctions.php
@@ -6,7 +6,7 @@
     <div class="MACTable">
       <table class="PeoplesCountTable">
         <tr>
-          
+         
           <?php $jobCapitalized = ucfirst($job); ?>
           <th class="PeoplesCountRowHeader"><?php echo "Total {$jobCapitalized}s"; ?></th>
 
@@ -15,14 +15,14 @@
               $query = $newconnection->prepare("SELECT COUNT(*) peoplecount FROM peoplesjobs INNER JOIN peoples ON peoples.PeopleID = peoplesjobs.PeopleID INNER JOIN jobs ON jobs.JobID = peoplesjobs.JobID INNER JOIN cities ON cities.CityID = peoples.BirthCityID WHERE City = ? AND Jobs = ? AND FirstName IS NOT NULL ");
               
               $query->bind_param("ss", $city, $job);
-              $query->execute();         
+              $query->execute();
 
               //Result variable with an error check
               $result = $query->get_result()
                 or die("Database query failed.");
 
               // 3. Use returned data (if any)
-              while($peoples = mysqli_fetch_assoc($result)) {
+              while ($peoples = mysqli_fetch_assoc($result)) {
                   // output data from each row
           ?>
 
@@ -31,7 +31,7 @@
 
           <?php
               }
-      
+
               // 4. Release returned data
               mysqli_free_result($result);
           ?>
