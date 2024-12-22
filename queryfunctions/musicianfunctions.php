806352b6c36cbfdf3582038b0964c08758c1826e
SeaFilmz <danielschmidt18@gmail.com>
2024-12-21 22:54:29 -0700

    Create a Function to Display the Total Number of Musicians Born in a City

---
+++ queryfunctions/musicianfunctions.php
@@ -0,0 +1,42 @@
+<?php
+  function cityMusiciansCount($city, $job) {
+    global $newconnection;
+?>
+
+    <div class="MACTable">
+      <table class="PeoplesCountTable">
+        <tr>
+
+          <?php $jobCapitalized = ucfirst($job); ?>
+          <th class="PeoplesCountRowHeader"><?php echo "Total {$jobCapitalized}s"; ?></th>
+
+          <?php
+              // 2. Perform database query
+              $query = $newconnection->prepare("SELECT COUNT(*) peoplecount FROM peoplesjobs INNER JOIN peoples ON peoples.PeopleID = peoplesjobs.PeopleID INNER JOIN jobs ON jobs.JobID = peoplesjobs.JobID INNER JOIN cities ON cities.CityID = peoples.BirthCityID WHERE City = ? AND Jobs = ? AND MusicianName IS NOT NULL ");
+
+              $query->bind_param("ss", $city, $job);
+              $query->execute();
+
+              //Result variable with an error check
+              $result = $query->get_result()
+                or die("Database query failed.");
+
+              // 3. Use returned data (if any)
+              while ($peoples = mysqli_fetch_assoc($result)) {
+                  // output data from each row
+          ?>
+
+          <td class="PeoplesCountNumber"><?php echo $peoples["peoplecount"]; ?></td>
+      </tr>
+
+          <?php
+              }
+
+              // 4. Release returned data
+              mysqli_free_result($result);
+          ?>
+
+      </table>
+    </div>
+
+  <?php  } ?>
