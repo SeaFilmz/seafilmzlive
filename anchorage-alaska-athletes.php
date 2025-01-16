c106f4c8ca8cf77af3d0a602b85274d6dc00ad0d
SeaFilmz <danielschmidt18@gmail.com>
2025-01-16 12:48:28 -0800

    Add the Webpage for Athletes Born in Anchorage, Alaska

---
+++ anchorage-alaska-athletes.php
@@ -0,0 +1,76 @@
+<!--link to the start of a seafilmz general webpage template-->
+<?php
+  $title = 'Anchorage, Alaska Athletes - SeaFilmz';
+  $mDesc = 'List of athletes born in the city of Anchorage organized by sport then by first name.';
+  $body = 'MainBody';
+  require_once 'sftemplate.php';
+  headerTemp();
+?>
+
+    <p class="AthletesPageHeader">
+      <b>
+        <a href="anchorage-alaska-athletes-beta">New Athletes Data UI Beta</a>
+      </b>
+    </p>
+
+    <p class="AthletesPageHeader">
+      <b>
+        <a href="anchorage-alaska-athletes-dataviz">Anchorage Born Athletes Dataviz</a>
+      </b>
+    </p>
+
+    <h1 class="AthletesPageHeader"><b>Athletes Born in Anchorage</b></h1>
+
+    <div class="ATable">
+    <table class="AthletesTable">
+      <tr>
+        <th class="AthletesColumnHeader1">Name</th>
+        <th class="AthletesColumnHeader2">Sport Known For</th>
+      </tr>
+
+        <?php
+            // 2. Perform database query
+            $query = $newconnection->prepare("SELECT * FROM peoplesjobs INNER JOIN peoples ON peoples.PeopleID = peoplesjobs.PeopleID INNER JOIN jobs ON jobs.JobID = peoplesjobs.JobID INNER JOIN cities ON cities.CityID = peoples.BirthCityID WHERE City = ? AND Jobs = ? ORDER BY SportKnownFor ASC, FirstName ");
+
+            $city = 'Anchorage';
+            $job = 'athlete';
+            $query->bind_param("ss", $city, $job);
+            $query->execute();
+
+            //Result variable with an error check
+            $result = $query->get_result()
+              or die("Database query failed.");
+
+            // 3. Use returned data (if any)
+            while($athletes = mysqli_fetch_assoc($result)) {
+                // output data from each row
+        ?>
+
+      <tr class="AthletesContent">
+        <td class="AthletesNameContent"> <b class="AthletesPageContent"> <a href= "<?php echo $athletes["PeopleLinks"]; ?>"> <?php echo $athletes["FirstName"]; ?> <?php echo $athletes["LastName"]; ?></a> </b></td>
+        <td class="SportPlayed"><?php echo $athletes["SportKnownFor"]; ?></td>
+      </tr>
+
+        <?php
+            }
+
+            // 4. Release returned data
+            mysqli_free_result($result);
+        ?>
+
+    </table>
+    </div>
+
+
+    <!--link to Total Athletes Count-->
+    <?php
+  require 'queryfunctions/athletefunctions.php';
+  require 'queryfunctions/peoplefunctions.php';
+  peopleCityBornByJobCount('Anchorage', 'athlete');
+?>
+
+
+<?php
+  // footer display function
+  footerTemp();
+?>
