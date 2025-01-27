e44bd6da77314e6dbb750ad5a8cd722a08690258
SeaFilmz <danielschmidt18@gmail.com>
2025-01-27 13:16:44 -0800

    Change City Movie Theater Code to Pull List of Movie Theaters from Database

--- portland-oregon-movie-theaters.php
+++ portland-oregon-movie-theaters.php
@@ -9,29 +9,13 @@
   headerTemp();
 ?>
 
-	<h1 class="SeattleMTHeader">Portland, Oregon Movie Theaters</h1>
+		<h1 class="SeattleMTHeader">Portland, Oregon Movie Theaters</h1>
 		<div class="SeattleMTContent">
       <ul class="SMTList">
-				<li class="SMTElements"><a href= "http://www.5thavecinema.com/">5th Avenue Cinema</a></li>
-				<li class="SMTElements"><a href= "https://academytheaterpdx.com/">Academy Theater</a></li>
-				<li class="SMTElements"><a href= "https://wunderlandgames.com/movies/avalon/">Avalon Theatre</a></li>
-				<li class="SMTElements"><a href= "https://www.mcmenamins.com/bagdad-theater-pub">Bagdad Theater & Pub</a></li>
-				<li class="SMTElements"><a href= "https://www.cinema21.com/">Cinema 21</a></li>
-				<li class="SMTElements"><a href= "https://www.thecinemagictheater.com/">CineMagic</a></li>
-				<li class="SMTElements"><a href= "https://www.cinemark.com/theatres/or-portland/cinemark-century-eastport-plaza-16">Cinemark Century Eastport Plaza 16</a></li>
-				<li class="SMTElements"><a href= "https://cstpdx.com/">Clinton Street Theater</a></li>
-				<li class="SMTElements"><a href= "https://omsi.edu/exhibits/empirical-theater/">Empirical Theater</a></li>
-				<li class="SMTElements"><a href= "https://hollywoodtheatre.org/">Hollywood Theatre</a></li>
-				<li class="SMTElements"><a href= "https://jubitz.com/dining-entertainment/jubitz-cinema/">Jubitz Cinema</a></li>
-				<li class="SMTElements"><a href= "https://www.mcmenamins.com/kennedy-school/kennedy-school-theater">Kennedy School Theater</a></li>
-				<li class="SMTElements"><a href= "https://www.laurelhursttheater.com/">Laurelhurst Theater and Pub</a></li>
-				<li class="SMTElements"><a href= "https://pdx.livingroomtheaters.com/nowplaying">Living Room Theaters</a></li>
-				<li class="SMTElements"><a href= "https://morelandtheater.com/">Moreland Theatre</a></li>
-				<li class="SMTElements"><a href= "https://www.regmovies.com/theatres/regal-division-street">Regal Division Street</a></li>
-				<li class="SMTElements"><a href= "https://www.regmovies.com/theatres/regal-fox-tower">Regal Fox Tower</a></li>
-				<li class="SMTElements"><a href= "https://www.regmovies.com/theatres/regal-lloyd-center-imax">Regal Lloyd Center & IMAX</a></li>
-				<li class="SMTElements"><a href= "https://www.mcmenamins.com/st-johns-theater-pub">St. Johns Theater and Pub</a></li>
-				<li class="SMTElements"><a href= "https://www.stjohnscinema.com/">St. Johns Twin Cinema and Pub</a></li>
+        <?php
+          require_once 'queryfunctions/cityfunctions.php';
+          cityAttractionQuery('Portland', 'Movie Theater');
+        ?>
       </ul>
 		</div>
 
--- seattle-movie-theaters.php
+++ seattle-movie-theaters.php
@@ -9,27 +9,14 @@
   headerTemp();
 ?>
 
-	<h1 class="SeattleMTHeader">Seattle Movie Theaters</h1>
+		<h1 class="SeattleMTHeader">Seattle Movie Theaters</h1>
 		<div class="SeattleMTContent">
       <ul class="SMTList">
-				<li class="SMTElements"><a href= "https://www.farawayentertainment.com/locations/admiral-theater">Admiral Theater</a></li>
-				<li class="SMTElements"><a href= "https://www.amctheatres.com/movie-theatres/seattle-tacoma/amc-oak-tree-6">AMC Loews Oak Tree 6</a></li>
-				<li class="SMTElements"><a href= "https://www.amctheatres.com/movie-theatres/seattle-tacoma/amc-pacific-place-11">AMC Pacific Place 11</a></li>
-				<li class="SMTElements"><a href= "https://www.amctheatres.com/movie-theatres/seattle-tacoma/amc-seattle-10">AMC Seattle 10</a></li>
-				<li class="SMTElements"><a href= "https://www.arklodgecinemas.com/">Ark Lodge Cinemas</a></li>
-				<li class="SMTElements"><a href= "https://central-cinema.com/">Central Cinema</a></li>
-				<li class="SMTElements"><a href= "https://pacificsciencecenter.org/visit/imax/">IMAX At Pacific Science Center</a></li>
-				<li class="SMTElements"><a href= "https://www.majesticbay.com/">Majestic Bay Theatres</a></li>
-				<li class="SMTElements"><a href= "https://www.regmovies.com/theatres/regal-meridian-4dx">Regal Meridian 16</a></li>
-				<li class="SMTElements"><a href= "https://www.regmovies.com/theatres/regal-thornton-place-imax">Regal Thornton Place Stadium 14 & IMAX</a></li>
-				<li class="SMTElements"><a href= "https://www.siff.net/cinema/cinema-venues/siff-cinema-downtown">SIFF Cinema Downtown</a></li>
-				<li class="SMTElements"><a href= "https://www.siff.net/cinema/cinema-venues/siff-cinema-egyptian">SIFF Cinema Egyptian</a></li>
-				<li class="SMTElements"><a href= "https://www.siff.net/cinema/cinema-venues/siff-cinema-uptown">SIFF Cinema Uptown</a></li>
-				<li class="SMTElements"><a href= "https://www.siff.net/cinema/cinema-venues/siff-film-center">SIFF Film Center</a></li>
-				<li class="SMTElements"><a href= "https://thebeacon.film/">The Beacon Cinema</a></li>
-				<li class="SMTElements"><a href= "https://grandillusioncinema.org/">The Grand Illusion</a></li>
-				<li class="SMTElements"><a href= "https://www.farawayentertainment.com/locations/varsity-theater">Varsity Theatre</a></li>
-    	</ul>
+        <?php
+          require_once 'queryfunctions/cityfunctions.php';
+          cityAttractionQuery('Seattle', 'Movie Theater');
+        ?>
+      </ul>
     </div>
 
     <!--link to footer-->
