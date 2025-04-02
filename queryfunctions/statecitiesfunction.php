1e384b7c5297c3c0dc1054a6658821bf44c5978d
SeaFilmz <danielschmidt18@gmail.com>
2025-04-02 16:06:30 -0700

    Center city page links on state webpages

--- queryfunctions/statecitiesfunction.php
+++ queryfunctions/statecitiesfunction.php
@@ -7,17 +7,17 @@
 
     $query->bind_param("s", $state);
     $query->execute();
-  
+
     //Result variable with an error check
     $result = $query->get_result()
       or die("Database query failed.");
 
     while ($city = mysqli_fetch_assoc($result)) { ?>
         <?php if ($city["CityLinks"] !== NULL) { ?>
-            <p class="WashingtonCitiesLink"><a href= "<?= $city["CityLinks"]; ?>"><?= $city["City"];
+            <p class="StateCitiesLinks"><a href= "<?= $city["CityLinks"]; ?>"><?= $city["City"];
         }
     }
-    
+
     mysqli_free_result($result);
   }
 ?>
