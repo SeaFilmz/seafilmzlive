2609ffb69d693b013feb22f703a48838845dc4ea
SeaFilmz <danielschmidt18@gmail.com>
2025-04-13 13:06:08 -0700

    Add name to search engine input text box

--- templates/moviesearchform.php
+++ templates/moviesearchform.php
@@ -3,7 +3,7 @@
 ?>
 
 	<form class="movieSearchForm">
-		<input type="text" placeholder="Movie Title" id="search" aria-label="search by movie title for movies filmed in Seattle, Washington or Portland, Oregon">
+		<input type="text" name="search" placeholder="Movie Title" id="search" aria-label="search by movie title for movies filmed in Seattle, Washington or Portland, Oregon">
 		<button type="submit" id="searchButton">Search</button>
 	</form>
 
