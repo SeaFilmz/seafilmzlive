628f8282bd233c49c4aacaa962523e21132991da
SeaFilmz <danielschmidt18@gmail.com>
2025-04-17 15:08:38 -0700

    Add header and footer to new movie search results page
    This page is not yet linked to the search box, and movie search results functionality is not implemented.

---
+++ movies-search.php
@@ -0,0 +1,13 @@
+<?php
+  $title = 'Search - SeaFilmz';
+  $mDesc = 'SeaFilmz movie title search results.';
+  $body = 'MainBody';
+  /*link to the start of a seafilmz general web page template*/
+  require_once 'sftemplate.php';
+  headerTemp();
+?>
+
+<?php
+  // footer display function
+  footerTemp();
+?>
