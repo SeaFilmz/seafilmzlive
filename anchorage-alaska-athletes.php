eede69743967f018829ba4cf5c761b99e9317044
SeaFilmz <danielschmidt18@gmail.com>
2025-05-10 22:11:03 -0700

    Rename athletefunctions.php file to athlete-functions.php to match SeaFilmz naming convention

--- anchorage-alaska-athletes.php
+++ anchorage-alaska-athletes.php
@@ -66,7 +66,7 @@
 
     <!--link to Total Athletes Count-->
     <?php
-  require 'queryfunctions/athletefunctions.php';
+  require 'queryfunctions/athlete-functions.php';
   require 'queryfunctions/people-functions.php';
   peopleCityBornByJobCount('Anchorage', 'athlete');
 ?>
