1709141caa46cd77d110db57249e7fe0ee81b8bb
SeaFilmz <danielschmidt18@gmail.com>
2025-06-09 17:09:52 -0700

    Rename CSS id search to Search to apply PascalCase
    Renamed to follow my new standard of using PascalCase for CSS id names.

--- templates/movie-search-form.php
+++ templates/movie-search-form.php
@@ -3,7 +3,7 @@
 ?>
 
 	<form action="movies-search" method="GET" class="MovieSearchForm">
-		<input type="text" name="search" placeholder="Movie Title" id="search" aria-label="search by movie title for movies filmed in Seattle, Washington or Portland, Oregon">
+		<input type="text" name="search" placeholder="Movie Title" id="Search" aria-label="search by movie title for movies filmed in Seattle, Washington or Portland, Oregon">
 		<button type="submit" id="searchButton">Search</button>
 	</form>
 
