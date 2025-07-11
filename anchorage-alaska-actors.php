04a07de022357c8f55e509bb9f04604dc75cee1c
SeaFilmz <danielschmidt18@gmail.com>
2025-07-10 18:55:48 -0700

    Add webpage for actors born in Anchorage, Alaska

---
+++ anchorage-alaska-actors.php
@@ -0,0 +1,118 @@
+<!--link to the start of a seafilmz general webpage template-->
+<?php
+  $title = 'Anchorage, Alaska Actors by First Name or by Birthdate - SeaFilmz';
+  $mDesc = 'List of actors born is the city of Anchorage, Alaska organized by first name or by birthdate.';
+  $ogTitle = 'Anchorage, Alaska Actors by First Name or by Birthdate - SeaFilmz';
+  $ogURL = 'https://seafilmz.com/anchorage-alaska-actors';
+  $body = 'MainBody';
+  require_once 'templates/main-page-structure.php';
+  headerTemp();
+?>
+
+    <p class="ActorsPageHeader">
+      <b>
+        <a href="anchorage-alaska-actors-beta">New Actors Data UI Beta</a>
+      </b>
+    </p>
+
+    <h1 id="sortByActorName" class="ActorsPageHeader"><b>Actors Born in Anchorage, Alaska by First Name</b></h1>
+
+    <div class="MATable">
+    <table class="ActorsTable">
+      <tr>
+        <th class="ActorsColumnHeader1">Name</th>
+        <th class="ActorsColumnHeader2"><a href="#sortByBirthdate" class="SortText">Birthdate</a><div class="SortTriangle">&#9660</div></th>
+      </tr>
+
+        <?php
+            // 2. Perform database query
+            $query = $newConnection->prepare("SELECT * FROM peoples_jobs INNER JOIN peoples ON peoples.people_id = peoples_jobs.people_id INNER JOIN jobs ON jobs.job_id = peoples_jobs.job_id INNER JOIN cities ON cities.city_id = peoples.birth_city_id WHERE city = ? AND job = ? AND first_name IS NOT NULL ORDER BY first_name ASC ");
+
+            $city = 'Anchorage';
+            $job = 'actor';
+            $query->bind_param("ss", $city, $job);
+            $query->execute();
+
+            //Result variable with an error check
+            $result = $query->get_result()
+              or die("Database query failed.");
+
+            // 3. Use returned data (if any)
+            while ($actors = mysqli_fetch_assoc($result)) {
+                // output data from each row
+        ?>
+
+      <tr class="ActorsMainContent">
+        <td class="ActorsNameContent"><b class="ActorsFirstName"> <a href= "<?php echo $actors["people_links"]; ?>"> <?php echo $actors["first_name"]; ?> <?php echo $actors["middle_initial_name"]; ?> <?php echo $actors["last_name"]; ?></a></b></td>
+        <td class="ActorsBirthdateContent"><?php $date = date_create($actors["birthdate"]); echo date_format($date, "M d, Y"); ?></td>
+      </tr>
+
+        <?php
+            }
+        ?>
+
+        <?php
+            // 4. Release returned data
+            mysqli_free_result($result);
+        ?>
+
+      </table>
+      </div>
+
+    <!--link to Total Actors Count-->
+<?php
+  require 'queryfunctions/people-functions.php';
+  peopleCityBornByJobCount('Anchorage', 'actor');
+?>
+
+    <h1 id="sortByBirthdate" class="ActorsPageHeader"><b>Actors Born in Seattle by Birthdate</b></h1>
+
+    <div class="MATable">
+    <table class="ActorsTable">
+      <tr>
+        <th class="ActorsColumnHeader1"><a href="#sortByActorName" class="SortText">Name</a><div class="SortTriangle">&#9650</div></th>
+        <th class="ActorsColumnHeader2">Birthdate</th>
+      </tr>
+
+        <?php
+            // 2. Perform database query
+            $query = $newConnection->prepare("SELECT * FROM peoples_jobs INNER JOIN peoples ON peoples.people_id = peoples_jobs.people_id INNER JOIN jobs ON jobs.job_id = peoples_jobs.job_id INNER JOIN cities ON cities.city_id = peoples.birth_city_id WHERE city = ? AND job = ? AND first_name IS NOT NULL ORDER BY birthdate DESC ");
+
+            $cityBirthdate = 'Anchorage';
+            $jobBirthdate = 'actor';
+            $query->bind_param("ss", $cityBirthdate, $jobBirthdate);
+            $query->execute();
+
+            //Result variable with an error check
+            $result = $query->get_result()
+              or die("Database query failed.");
+
+            // 3. Use returned data (if any)
+            while ($actors = mysqli_fetch_assoc($result)) {
+                // output data from each row
+        ?>
+
+      <tr class="ActorsMainContent">
+        <td class="ActorsNameContent"> <b class="ActorsFirstName"> <a href= "<?php echo $actors["people_links"]; ?>"> <?php echo $actors["first_name"]; ?> <?php echo $actors["middle_initial_name"]; ?> <?php echo $actors["last_name"]; ?></a></b></td>
+        <td class="ActorsBirthdateContent"><?php $date = date_create($actors["birthdate"]); echo date_format($date, "M d, Y"); ?></td>
+      </tr>
+
+        <?php
+            }
+        ?>
+
+        <?php
+            // 4. Release returned data
+            mysqli_free_result($result);
+        ?>
+
+    </table>
+    </div>
+
+    <!--link to Total Actors Count-->
+<?php peopleCityBornByJobCount('Anchorage', 'actor'); ?>
+
+<?php
+  // footer display function
+  footerTemp();
+?>
