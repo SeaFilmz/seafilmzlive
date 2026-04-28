b4a412dbf2ff55e76931b17c47871561b5d1cafa
SeaFilmz <danielschmidt18@gmail.com>
2026-04-27 20:51:04 -0700

    Expand actor search to include middle names and initials
    Updated the search query to check the middle_initial_name column and implemented parentheses to ensure regional filtering remains accurate across all name fields.

--- movies-search.php
+++ movies-search.php
@@ -27,8 +27,9 @@
 			UNION
 			SELECT CONCAT_WS(' ', first_name, middle_initial_name, last_name) AS DisplayName, 'N/A' AS YearReleased, people_links AS Link, job AS Job, 2 AS SortPriority FROM peoples_jobs INNER JOIN peoples ON peoples.people_id = peoples_jobs.people_id INNER JOIN jobs ON jobs.job_id = peoples_jobs.job_id INNER JOIN cities ON peoples.birth_city_id = cities.city_id
         WHERE job = 'actor'
-        AND REPLACE(CONCAT(first_name, IFNULL(middle_initial_name, ''), last_name), ' ', '') LIKE ?
+        AND (REPLACE(CONCAT(first_name, IFNULL(middle_initial_name, ''), last_name), ' ', '') LIKE ?
         OR last_name LIKE ?
+        OR middle_initial_name LIKE ?)
         AND state_province IN ('Washington', 'Oregon', 'Idaho','Alaska')
         ORDER BY SortPriority ASC, DisplayName ASC
 		");
