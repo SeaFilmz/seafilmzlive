be46bf8c8de595036ec0d1d2a9eecdac0b82d78b
SeaFilmz <danielschmidt18@gmail.com>
2026-05-01 22:51:40 -0700

    Add hover transition to SeaFilmz Quiz button
    Implemented a CSS hover state to provide visual feedback on the quiz initiation button to try to help improve interactivity and user engagement.

--- index.php
+++ index.php
@@ -43,7 +43,7 @@
       </div>
 
 			<div class="quizStartButton">
-        <button type="button" onclick="startQuizButton()" class="SFStartQuizButton"><b>Start SeaFilmz Quiz</b></button>
+        <button type="button" onclick="startQuizButton()" class="SFStartQuizButton SFStartQuizButtonHover"><b>Start SeaFilmz Quiz</b></button>
       </div>
 
       <section class="SFQuizBackground">
