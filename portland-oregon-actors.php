17f15ed48d7c235fbb58834331445e3e55114bb9
SeaFilmz <danielschmidt18@gmail.com>
2026-05-22 13:54:33 -0700

    Extract repetitive strings to global variables in Portland, Oregon born actors tables
    Converted multiple instances of the strings 'Portland' and 'actor' into global variables for the main tables in the Portland, Oregon Born Actors file.

--- portland-oregon-actors.php
+++ portland-oregon-actors.php
@@ -25,11 +25,13 @@
       </tr>
 
         <?php
+            // 1. Declare Global Variables
+            $city = 'Portland';
+            $job = 'actor';
+
             // 2. Perform database query
             $query = $newConnection->prepare("SELECT * FROM peoples_jobs INNER JOIN peoples ON peoples.people_id = peoples_jobs.people_id INNER JOIN jobs ON  = peoples_jobs.job_id INNER JOIN cities ON  = peoples.birth_city_id WHERE city = ? AND job = ? AND first_name IS NOT NULL ORDER BY first_name ASC ");
 
-            $city = 'Portland';
-            $job = 'actor';
             $query->bind_param("ss", $city, $job);
             $query->execute();
 
@@ -78,9 +80,7 @@
             // 2. Perform database query
             $query = $newConnection->prepare("SELECT * FROM peoples_jobs INNER JOIN peoples ON peoples.people_id = peoples_jobs.people_id INNER JOIN jobs ON  = peoples_jobs.job_id INNER JOIN cities ON  = peoples.birth_city_id WHERE city = ? AND job = ? AND first_name IS NOT NULL ORDER BY Birthdate DESC ");
 
-            $cityBirthdate = 'Portland';
-            $jobBirthdate = 'actor';
-            $query->bind_param("ss", $cityBirthdate, $jobBirthdate);
+            $query->bind_param("ss", $city, $job);
             $query->execute();
 
             //Result variable with an error check
