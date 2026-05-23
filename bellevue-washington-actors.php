7b6c8a1fe8751630791b8b7acfd8ef1996f58253
SeaFilmz <danielschmidt18@gmail.com>
2026-05-23 15:22:07 -0700

    Extract repetitive strings to global variables for Bellevue, Washington born actors tables
    Converted multiple instances of the strings 'Bellevue' and 'actor' into global variables for the main tables in the Bellevue, Washington Born Actors file.

--- bellevue-washington-actors.php
+++ bellevue-washington-actors.php
@@ -24,11 +24,13 @@
       </tr>
 
         <?php
+            // 1. Declare Global Variables
+            $city = 'Bellevue';
+            $job = 'actor';
+
             // 2. Perform database query
             $query = $newConnection->prepare("SELECT * FROM peoples_jobs INNER JOIN peoples ON peoples.people_id = peoples_jobs.people_id INNER JOIN jobs ON  = peoples_jobs.job_id INNER JOIN cities ON  = peoples.birth_city_id WHERE city = ? AND job = ? AND first_name IS NOT NULL ORDER BY first_name ASC ");
 
-            $city = 'Bellevue';
-            $job = 'actor';
             $query->bind_param("ss", $city, $job);
             $query->execute();
 
@@ -77,9 +79,7 @@
             // 2. Perform database query
             $query = $newConnection->prepare("SELECT * FROM peoples_jobs INNER JOIN peoples ON peoples.people_id = peoples_jobs.people_id INNER JOIN jobs ON  = peoples_jobs.job_id INNER JOIN cities ON  = peoples.birth_city_id WHERE city = ? AND job = ? AND first_name IS NOT NULL ORDER BY Birthdate DESC ");
 
-            $cityBirthdate = 'Bellevue';
-            $jobBirthdate = 'actor';
-            $query->bind_param("ss", $cityBirthdate, $jobBirthdate);
+            $query->bind_param("ss", $city, $job);
             $query->execute();
 
             //Result variable with an error check
