ac028af567f43d97e8b00e244b41b661effc2e17
SeaFilmz <danielschmidt18@gmail.com>
2026-05-22 22:02:38 -0700

    Pass global variables into city born people by job count function calls
    Updated the peopleCityBornByJobCount function calls in the Anchorage, Alaska Born Actors file to use the $city and $job variables instead of hard-coded strings.

--- anchorage-alaska-actors.php
+++ anchorage-alaska-actors.php
@@ -64,7 +64,7 @@
     <!--link to Total Actors Count-->
 <?php
   require_once 'queryfunctions/people-functions.php';
-  peopleCityBornByJobCount('Anchorage', 'actor');
+  peopleCityBornByJobCount($city, $job);
 ?>
 
     <h2 id="sortByBirthdate" class="ActorsPageHeader"><b>Actors Born in Seattle by Birthdate</b></h2>
@@ -110,7 +110,7 @@
     </div>
 
     <!--link to Total Actors Count-->
-<?php peopleCityBornByJobCount('Anchorage', 'actor'); ?>
+<?php peopleCityBornByJobCount($city, $job); ?>
 
 <?php
   // footer display function
