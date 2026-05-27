63af76b04fe323225dacf2c76c3ef5e92ac5c549
SeaFilmz <danielschmidt18@gmail.com>
2026-05-26 19:53:01 -0700

    Make function parameters dynamic using city and job variables in Portland, Oregon born athletes
    Updated the peopleCityBornByJobCount function call in the Portland, Oregon Born Athletes file to pass the $city and $job variables as parameters instead of the hard-coded strings.

--- portland-oregon-athletes.php
+++ portland-oregon-athletes.php
@@ -31,11 +31,13 @@
       </tr>
 
         <?php
+            // 1. Declare Global Variables
+            $city = 'Portland';
+            $job = 'athlete';
+
             // 2. Perform database query
             $query = $newConnection->prepare("SELECT * FROM peoples_jobs INNER JOIN peoples ON peoples.people_id = peoples_jobs.people_id INNER JOIN jobs ON  = peoples_jobs.job_id INNER JOIN cities ON  = peoples.birth_city_id WHERE city = ? AND job = ? ORDER BY sport_known_for ASC, first_name ");
 
-            $city = 'Portland';
-            $job = 'athlete';
             $query->bind_param("ss", $city, $job);
             $query->execute();
 
@@ -66,7 +68,7 @@
     <!--link to Total Athletes Count-->
 <?php
   require_once 'queryfunctions/people-functions.php';
-  peopleCityBornByJobCount('Portland', 'athlete');
+  peopleCityBornByJobCount($city, $job);
 ?>
 
 <?php
