40cbf814f9690f68d6e37e6db7f88d08eb9c73ca
SeaFilmz <danielschmidt18@gmail.com>
2026-06-06 17:29:48 -0700

    Restructure movie search script file and associated CSS
    Refactored the internal code structure of movie-search.php and cleaned up its accompanying CSS rules to improve readability, code organization, and structural alignment.

--- movies-search.php
+++ movies-search.php
@@ -23,16 +23,15 @@
 		$query = $newConnection->prepare(
 			"SELECT movie_title AS DisplayName, '' AS MusicanName, year_released AS YearReleased, movie_page_link AS Link, '' AS Job, 1 AS SortPriority FROM movies
 				WHERE REPLACE(movie_title, ' ', '') LIKE ?
-				ORDER BY movie_title ASC
 			UNION
 			SELECT CONCAT_WS(' ', first_name, middle_initial_name, last_name) AS DisplayName, musician_name AS MusicanName, 'N/A' AS YearReleased, people_links AS Link, job AS Job, 2 AS SortPriority FROM peoples_jobs INNER JOIN peoples ON peoples.people_id = peoples_jobs.people_id INNER JOIN jobs ON jobs.job_id = peoples_jobs.job_id INNER JOIN cities ON peoples.birth_city_id = cities.city_id
-        WHERE job in ('actor', 'athlete')
-        AND (REPLACE(CONCAT(first_name, IFNULL(middle_initial_name, ''), last_name), ' ', '') LIKE ?
-        OR last_name LIKE ?
-        OR middle_initial_name LIKE ?
-				OR musician_name LIKE ?)
-        AND state_province IN ('Washington', 'Oregon', 'Idaho','Alaska')
-        ORDER BY SortPriority ASC, DisplayName ASC
+        WHERE job in ('actor', 'musician', 'athlete')
+          AND (REPLACE(CONCAT(first_name, IFNULL(middle_initial_name, ''), last_name), ' ', '') LIKE ?
+          OR last_name LIKE ?
+          OR middle_initial_name LIKE ?
+          OR musician_name LIKE ?)
+          AND state_province IN ('Washington', 'Oregon', 'Idaho','Alaska')
+          ORDER BY SortPriority ASC, DisplayName ASC
 		");
 
 		$query->bind_param("sssss", $movieTitle, $peopleName, $peopleName, $peopleName, $peopleName);
@@ -54,36 +53,34 @@
 		<?php
 		}
 
-		if ($queryResults > 0 and $searchRemoveWhitespaceAll !== "") {
-			while ($row = mysqli_fetch_assoc($result)) {
+			if ($queryResults > 0 and $searchRemoveWhitespaceAll !== "") {
+				while ($row = mysqli_fetch_assoc($result)) {
 		?>
 
-				<div class="MovieResult">
+					<div class="SearchResult">
 
-				  <div class="MovieTitleYear"><a href= "<?php echo $row["Link"]; ?>" class="MovieTitleLink"><?php echo $row["DisplayName"]; ?></a></div>
-				  <?php if ($row["SortPriority"] == 1) { ?>
-    				<div><?php echo $row["YearReleased"]; ?></div>
-    				<div>Movie</div>
-    			  <?php } elseif ($row["Job"] == 'actor') { ?>
-    		      <div>Actor</div>
-    			  <?php } elseif ($row["Job"] == 'athlete') { ?>
-    		      <div>Athlete</div>
-    			  <?php } ?>
-
-				</div>
-
-				<div class="MovieTitleYear"><a href= "<?php echo $row["Link"]; ?>" class="MovieTitleLink"><?php echo $row["MusicanName"]; ?></a></div>
-
-    			<?php if ($row["Job"] == 'musician') { ?>
-    	      <div>Musician</div>
-    		  <?php } ?>
-
+						<?php if ($row["SortPriority"] == 1) { ?>
+							<div class="MoviePersonSearchResult"><a href= "<?php echo $row["Link"]; ?>" class="InternalSearchLink"><?php echo $row["DisplayName"]; ?></a></div>
+							<div><?php echo $row["YearReleased"]; ?></div>
+							<div>Movie</div>
+						<?php } elseif ($row["Job"] == 'actor') { ?>
+							<div class="MoviePersonSearchResult"><a href= "<?php echo $row["Link"]; ?>" class="InternalSearchLink"><?php echo $row["DisplayName"]; ?></a></div>
+							<div>Actor</div>
+						<?php } elseif ($row["Job"] == 'athlete') { ?>
+							<div class="MoviePersonSearchResult"><a href= "<?php echo $row["Link"]; ?>" class="InternalSearchLink"><?php echo $row["DisplayName"]; ?></a></div>
+							<div>Athlete</div>
+						<?php } elseif ($row["Job"] == 'musician') { ?>
+							<div class="MoviePersonSearchResult"><a href= "<?php echo $row["Link"]; ?>" class="InternalSearchLink"><?php echo $row["MusicianName"]; ?></a></div>
+							<div>Musician</div>
+						<?php } else {
+							echo "There are no results matching your search!";
+						} ?>
+					</div>
 				<?php
+				}
 			}
-		} else {
-			echo "There are no results matching your search!";
-		}
-		?>
+	}
+	?>
 
 		<div class="numberOfSearcResults">
 			<?php
@@ -103,7 +100,6 @@
 			?>
     </div>
 	<?php
-	}
 ?>
 
 </main>
