c7fe6577607670d1b3837c0f44a54cb0ffcd13bc
Daniel Schmidt <danielschmidt18@gmail.com>
2026-06-30 23:47:23 -0700

    Improve SeaFilmz search form aria-label
    This updates the search form aria-label to better describe the available search options.

--- templates/movie-search-form.php
+++ templates/movie-search-form.php
@@ -3,7 +3,7 @@
 ?>
 
 	<form action="movies-search" method="GET" class="MovieSearchForm">
-		<input type="text" name="search" placeholder="Movie Title or Actor Name or Musician Name or Athlete Name" id="Search" aria-label="search by movie title for movies filmed in Seattle, Washington or Portland, Oregon">
+		<input type="text" name="search" placeholder="Movie Title or Actor Name or Musician Name or Athlete Name" id="Search" aria-label="Search by movie title for movies filmed in Northwest United States or by actor name for actors born in the Northwest United States or by musician name for musicians born in the Northwest United States or by athlete name for athletes born in the Northwest United States">
 		<button type="submit" id="SearchButton">Search</button>
 	</form>
 
