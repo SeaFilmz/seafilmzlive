b1fc07000530ad738c27b18df9cd4d641205a281
user.seafilmz <danielschmidt18@gmail.com>
2026-07-19 19:41:18 -0700

    Change main content header from h2 to h1 for contact page
    Update the primary heading inside the main content section of the contact page to an h1 to improve semantic HTML structure and on-page SEO.

--- contact.php
+++ contact.php
@@ -10,7 +10,7 @@
 ?>
 
               <div class="Contact">
-                <h2>Contact Info</h2>
+                <h1>Contact Info</h1>
                 <b class="contactTitle1">Seattle Filmz Email</b> :
                 <span class="contactInfo">seafilmz@gmail.com</span>
                 <p></p>
