6af538f257afb6cb20e7b627416d4be1135b3e8a
SeaFilmz <danielschmidt18@gmail.com>
2026-08-10 19:18:50 -0700

    Standardize CSS class names in city-details.php
    Updated the CSS class names in city-details.php from non-PascalCase to PascalCase to match the SeaFilmz CSS naming convention and improve code consistency and maintainability.

--- city-details.php
+++ city-details.php
@@ -41,9 +41,9 @@
 
       <table class="CityTable">
         <?php if ($cityFact["incorporated_date"] !== NULL) { ?>
-          <tr class="cityDataPointRow">
-            <td class="cityData cityDataDesc">Officially Became a City</td>
-            <td class="cityData"><?php $date = date_create($cityFact["incorporated_date"]); echo date_format($date, "F d, Y"); ?></td>
+          <tr class="CityDataPointRow">
+            <td class="CityData CityDataDesc">Officially Became a City</td>
+            <td class="CityData"><?php $date = date_create($cityFact["incorporated_date"]); echo date_format($date, "F d, Y"); ?></td>
           </tr>
         <?php } ?>
 
@@ -56,55 +56,55 @@
 
         <?php if ($rows === 1) { ?>
           <?php if ($StateProvince !== 'Alaska') { ?>
-            <tr class="cityDataPointRow">
-              <td class="cityData cityDataDesc">County</td>
-              <td class="cityData"><a href="<?= "{$cityFact["official_county_links"]}"; ?>" target="_blank"><?= "{$cityFact["county"]}"; ?></a></td>
+            <tr class="CityDataPointRow">
+              <td class="CityData CityDataDesc">County</td>
+              <td class="CityData"><a href="<?= "{$cityFact["official_county_links"]}"; ?>" target="_blank"><?= "{$cityFact["county"]}"; ?></a></td>
             </tr>
           <?php } elseif ($StateProvince === 'Alaska') { ?>
-            <tr class="cityDataPointRow">
-              <td class="cityData cityDataDesc">Borough</td>
-              <td class="cityData"><a href="<?= "{$cityFact["official_county_links"]}"; ?>" target="_blank"><?= "{$cityFact["county"]}"; ?></a></td>
+            <tr class="CityDataPointRow">
+              <td class="CityData CityDataDesc">Borough</td>
+              <td class="CityData"><a href="<?= "{$cityFact["official_county_links"]}"; ?>" target="_blank"><?= "{$cityFact["county"]}"; ?></a></td>
             </tr>
           <?php }
         } elseif ($city === 'Bothell') { ?>
-          <tr class="cityDataPointRow">
-            <td class="cityData cityDataDesc">County</td>
-            <td class="cityData">
-              <div class="cityDataPointOfMany"><a href="https://kingcounty.gov/" target="_blank">King</a></div>
+          <tr class="CityDataPointRow">
+            <td class="CityData CityDataDesc">County</td>
+            <td class="CityData">
+              <div class="CityDataPointOfMany"><a href="https://kingcounty.gov/" target="_blank">King</a></div>
               <div><a href="https://snohomishcountywa.gov/" target="_blank">Snohomish</a></div>
             </td>
           </tr>
         <?php } elseif ($city === 'Portland') { ?>
-          <tr class="cityDataPointRow">
-            <td class="cityData cityDataDesc">County</td>
-            <td class="cityData">
-              <div class="cityDataPointOfMany"><a href="https://multco.us/" target="_blank">Multnomah</a></div>
-              <div class="cityDataPointOfMany"><a href="https://www.clackamas.us/" target="_blank">Clackamas</a></div>
-              <div><a href="https://www.co.washington.or.us/" target="_blank">Washington</a></div>
+          <tr class="CityDataPointRow">
+            <td class="CityData CityDataDesc">County</td>
+            <td class="CityData">
+              <div class="CityDataPointOfMany"><a href="https://multco.us/" target="_blank">Multnomah</a></div>
+              <div class="CityDataPointOfMany"><a href="https://www.clackamas.us/" target="_blank">Clackamas</a></div>
+              <div class="CityDataPointOfMany"><a href="https://www.co.washington.or.us/" target="_blank">Washington</a></div>
             </td>
           </tr>
         <?php } elseif ($city === 'Milwaukie') { ?>
-          <tr class="cityDataPointRow">
-            <td class="cityData cityDataDesc">County</td>
-            <td class="cityData">
-              <div class="cityDataPointOfMany"><a href="https://multco.us/" target="_blank">Multnomah</a></div>
-              <div><a href="https://www.clackamas.us/" target="_blank">Clackamas</a></div>
+          <tr class="CityDataPointRow">
+            <td class="CityData CityDataDesc">County</td>
+            <td class="CityData">
+              <div class="CityDataPointOfMany"><a href="https://multco.us/" target="_blank">Multnomah</a></div>
+              <div class="CityDataPointOfMany"><a href="https://www.clackamas.us/" target="_blank">Clackamas</a></div>
             </td>
           </tr>
         <?php } elseif ($city === 'Salem') { ?>
-          <tr class="cityDataPointRow">
-            <td class="cityData cityDataDesc">County</td>
-            <td class="cityData">
-              <div class="cityDataPointOfMany"><a href="https://www.co.marion.or.us/" target="_blank">Marion</a></div>
-              <div><a href="https://www.co.polk.or.us/" target="_blank">Polk</a></div>
+          <tr class="CityDataPointRow">
+            <td class="CityData CityDataDesc">County</td>
+            <td class="CityData">
+              <div class="CityDataPointOfMany"><a href="https://www.co.marion.or.us/" target="_blank">Marion</a></div>
+              <div class="CityDataPointOfMany"><a href="https://www.co.polk.or.us/" target="_blank">Polk</a></div>
             </td>
           </tr>
         <?php } elseif ($city === 'Pocatello') { ?>
-          <tr class="cityDataPointRow">
-            <td class="cityData cityDataDesc">County</td>
-            <td class="cityData">
-              <div class="cityDataPointOfMany"><a href="https://www.bannockcounty.us/" target="_blank">Bannock</a></div>
-              <div><a href="https://www.co.power.id.us/" target="_blank">Power</a></div>
+          <tr class="CityDataPointRow">
+            <td class="CityData CityDataDesc">County</td>
+            <td class="CityData">
+              <div class="CityDataPointOfMany"><a href="https://www.bannockcounty.us/" target="_blank">Bannock</a></div>
+              <div class="CityDataPointOfMany"><a href="https://www.co.power.id.us/" target="_blank">Power</a></div>
             </td>
           </tr>
         <?php } ?>
@@ -114,79 +114,79 @@
         } ?>
 
         <?php if ($city === 'Seattle') { ?>
-          <tr class="cityDataPointRow">
-            <td class="cityData cityDataDesc">Movie Theaters</td>
-            <td class="cityData"><a href="seattle-movie-theaters">List of Movie Theaters</a></td>
+          <tr class="CityDataPointRow">
+            <td class="CityData CityDataDesc">Movie Theaters</td>
+            <td class="CityData"><a href="seattle-movie-theaters">List of Movie Theaters</a></td>
           </tr>
-          <tr class="cityDataPointRow">
-            <td class="cityData cityDataDesc">Movies Filmed</td>
-            <td class="cityData"><a href="seattle-movies">List of Movies Filmed</a></td>
+          <tr class="CityDataPointRow">
+            <td class="CityData CityDataDesc">Movies Filmed</td>
+            <td class="CityData"><a href="seattle-movies">List of Movies Filmed</a></td>
           </tr>
-          <tr class="cityDataPointRow">
-            <td class="cityData cityDataDesc">Other Film and Media Resources for Seattle</td>
-            <td class="cityData">
-              <div class="cityDataPointOfMany"><a href="https://www.seattle.gov/filmandmusic" target="_blank">Seattle Office of Film and Music</a></div>
-              <div class="cityDataPointOfMany"><a href="https://www.washingtonfilmworks.org/" target="_blank">Washington Filmworks</a></div>
-              <div class="cityDataPointOfMany"><a href="https://www.siff.net/" target="_blank">Seattle International Film Festival</a></div>
-              <div><a href="https://nwfilmforum.org/" target="_blank">Northwest Film Forum</a></div>
+          <tr class="CityDataPointRow">
+            <td class="CityData CityDataDesc">Other Film and Media Resources for Seattle</td>
+            <td class="CityData">
+              <div class="CityDataPointOfMany"><a href="https://www.seattle.gov/filmandmusic" target="_blank">Seattle Office of Film and Music</a></div>
+              <div class="CityDataPointOfMany"><a href="https://www.washingtonfilmworks.org/" target="_blank">Washington Filmworks</a></div>
+              <div class="CityDataPointOfMany"><a href="https://www.siff.net/" target="_blank">Seattle International Film Festival</a></div>
+              <div class="CityDataPointOfMany"><a href="https://nwfilmforum.org/" target="_blank">Northwest Film Forum</a></div>
             </td>
           </tr>
-          <tr class="cityDataPointRow">
-            <td class="cityData cityDataDesc">People Born</td>
-            <td class="cityData">
-              <div class="cityDataPointOfMany"><a href="seattle-actors">Actors</a></div>
-              <div class="cityDataPointOfMany"><a href="seattle-athletes">Athletes</a></div>
-              <div class="cityDataPointOfMany"><a href="seattle-musicians">Musicians</a></div>
+          <tr class="CityDataPointRow">
+            <td class="CityData CityDataDesc">People Born</td>
+            <td class="CityData">
+              <div class="CityDataPointOfMany"><a href="seattle-actors">Actors</a></div>
+              <div class="CityDataPointOfMany"><a href="seattle-athletes">Athletes</a></div>
+              <div class="CityDataPointOfMany"><a href="seattle-musicians">Musicians</a></div>
             </td>
           </tr>
-          <tr class="cityDataPointRow">
-            <td class="cityData cityDataDesc">Professional Sports Teams</td>
-            <td class="cityData">
-              <div class="cityDataPointOfMany"><a href="http://www.seahawks.com/" target="_blank">Seattle Seahawks</a> (men's football)</div>
-              <div class="cityDataPointOfMany"><a href="https://www.mlb.com/mariners" target="_blank">Seattle Mariners</a> (men's baseball)</div>
-              <div class="cityDataPointOfMany"><a href="https://www.soundersfc.com/" target="_blank">Seattle Sounders FC</a> (men's soccer)</div>
-              <div class="cityDataPointOfMany"><a href="https://www.nhl.com/kraken" target="_blank">Seattle Kraken</a> (men's hockey)</div>
-              <div class="cityDataPointOfMany"><a href="http://storm.wnba.com/" target="_blank">Seattle Storm</a> (women's basketball)</div>
-              <div class="cityDataPointOfMany"><a href="https://www.reignfc.com/" target="_blank">Seattle Reign FC</a> (women's soccer)</div>
-              <div class="cityDataPointOfMany"><a href="https://www.thepwhl.com/en/teams/seattle-torrent" target="_blank">Seattle Torrent</a> (women's hockey)</div>
+          <tr class="CityDataPointRow">
+            <td class="CityData CityDataDesc">Professional Sports Teams</td>
+            <td class="CityData">
+              <div class="CityDataPointOfMany"><a href="http://www.seahawks.com/" target="_blank">Seattle Seahawks</a> (men's football)</div>
+              <div class="CityDataPointOfMany"><a href="https://www.mlb.com/mariners" target="_blank">Seattle Mariners</a> (men's baseball)</div>
+              <div class="CityDataPointOfMany"><a href="https://www.soundersfc.com/" target="_blank">Seattle Sounders FC</a> (men's soccer)</div>
+              <div class="CityDataPointOfMany"><a href="https://www.nhl.com/kraken" target="_blank">Seattle Kraken</a> (men's hockey)</div>
+              <div class="CityDataPointOfMany"><a href="http://storm.wnba.com/" target="_blank">Seattle Storm</a> (women's basketball)</div>
+              <div class="CityDataPointOfMany"><a href="https://www.reignfc.com/" target="_blank">Seattle Reign FC</a> (women's soccer)</div>
+              <div class="CityDataPointOfMany"><a href="https://www.thepwhl.com/en/teams/seattle-torrent" target="_blank">Seattle Torrent</a> (women's hockey)</div>
             </td>
           </tr>
           <?php cityAttractionTableQuery($city, 'Major League Sports Venue'); ?>
-          <tr class="cityDataPointRow">
-            <td class="cityData cityDataDesc">Libraries</td>
-            <td class="cityData"><a href="https://www.spl.org/hours-and-locations">List of Libraries</a></td>
+          <tr class="CityDataPointRow">
+            <td class="CityData CityDataDesc">Libraries</td>
+            <td class="CityData"><a href="https://www.spl.org/hours-and-locations">List of Libraries</a></td>
           </tr>
         <?php } ?>
 
         <?php if ($city === 'Portland') { ?>
-          <tr class="cityDataPointRow">
-            <td class="cityData cityDataDesc">Movie Theaters</td>
-            <td class="cityData"><a href="portland-oregon-movie-theaters">List of Movie Theaters</a></td>
+          <tr class="CityDataPointRow">
+            <td class="CityData CityDataDesc">Movie Theaters</td>
+            <td class="CityData"><a href="portland-oregon-movie-theaters">List of Movie Theaters</a></td>
           </tr>
-          <tr class="cityDataPointRow">
-            <td class="cityData cityDataDesc">Movies Filmed</td>
-            <td class="cityData"><a href="portland-oregon-movies">List of Movies Filmed</a></td>
+          <tr class="CityDataPointRow">
+            <td class="CityData CityDataDesc">Movies Filmed</td>
+            <td class="CityData"><a href="portland-oregon-movies">List of Movies Filmed</a></td>
           </tr>
-          <tr class="cityDataPointRow">
-            <td class="cityData cityDataDesc">People Born</td>
-            <td class="cityData">
-              <div class="cityDataPointOfMany"><a href="portland-oregon-actors">Actors</a></div>
-              <div class="cityDataPointOfMany"><a href="portland-oregon-athletes">Athletes</a></div>
-              <div class="cityDataPointOfMany"><a href="portland-oregon-musicians">Musicians</a></div>
+          <tr class="CityDataPointRow">
+            <td class="CityData CityDataDesc">People Born</td>
+            <td class="CityData">
+              <div class="CityDataPointOfMany"><a href="portland-oregon-actors">Actors</a></div>
+              <div class="CityDataPointOfMany"><a href="portland-oregon-athletes">Athletes</a></div>
+              <div class="CityDataPointOfMany"><a href="portland-oregon-musicians">Musicians</a></div>
             </td>
           </tr>
-          <tr class="cityDataPointRow">
-            <td class="cityData cityDataDesc">Professional Sports Teams</td>
-            <td class="cityData">
-              <div class="cityDataPointOfMany"><a href="https://www.nba.com/blazers/" target="_blank">Portland Trail Blazers</a> (men's basketball)</div>
-              <div class="cityDataPointOfMany"><a href="https://www.timbers.com/" target="_blank">Portland Timbers</a> (men's soccer)</div>
-              <div><a href="https://www.thorns.com/" target="_blank">Portland Thorns FC</a> (women's soccer)</div>
+          <tr class="CityDataPointRow">
+            <td class="CityData CityDataDesc">Professional Sports Teams</td>
+            <td class="CityData">
+              <div class="CityDataPointOfMany"><a href="https://www.nba.com/blazers/" target="_blank">Portland Trail Blazers</a> (men's basketball)</div>
+              <div class="CityDataPointOfMany"><a href="https://www.timbers.com/" target="_blank">Portland Timbers</a> (men's soccer)</div>
+              <div class="CityDataPointOfMany"><a href="https://www.thorns.com/" target="_blank">Portland Thorns FC</a> (women's soccer)</div>
             </td>
           </tr>
           <?php cityAttractionTableQuery($city, 'Major League Sports Venue'); ?>
-          <tr class="cityDataPointRow">
-            <td class="cityData cityDataDesc">Libraries</td>
-            <td class="cityData"><a href="portland-oregon-libraries">List of Libraries</a></td>
+          <tr class="CityDataPointRow">
+            <td class="CityData CityDataDesc">Libraries</td>
+            <td class="CityData"><a href="portland-oregon-libraries">List of Libraries</a></td>
           </tr>
         <?php } ?>
 
