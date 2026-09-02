573768087a61e92b1754d7b5bf618b5f4fe019ad
SeaFilmz <danielschmidt18@gmail.com>
2026-09-01 22:36:08 -0700

    Use database county variable for Milwaukie's Multnomah County
    Updated the Milwaukie county display in city-details.php to use the database-driven $county variable for Multnomah County while keeping Clackamas County manually coded.

--- city-details.php
+++ city-details.php
@@ -88,7 +88,7 @@
           <tr class="CityDataPointRow">
             <td class="CityData CityDataDesc">County</td>
             <td class="CityData">
-              <div class="CityDataPointOfMany"><a href="https://multco.us/" target="_blank">Multnomah</a></div>
+              <div class="CityDataPointOfMany"><a href="https://multco.us/" target="_blank"><?= $county; ?></a></div>
               <div class="CityDataPointOfMany"><a href="https://www.clackamas.us/" target="_blank">Clackamas</a></div>
             </td>
           </tr>
